2be4711d09a4f18a24fe81317182b93607902ecd
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-03-25 23:46:48 -0300

    Desafio 02 completo

--- JavaScript/desafios/d003/script.js
+++ JavaScript/desafios/d003/script.js
@@ -1,3 +1,6 @@
 function clique(){
   var numero = prompt('Escolha um numero Inteiro')
+  var numant = numero - 1
+  var numdep = numant + 2
+  alert(`O numero anterior ao ${numero} é: ${numant}, e o posterior é: ${numdep}`)
 }
