0eccc37e876e4c8b6de7f2dc8b21deb25c40312c
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-03-25 22:47:30 -0300

    Desafio 02 completo

---
+++ JavaScript/desafios/d002/script.js
@@ -0,0 +1,6 @@
+
+function clique(){
+  var nome = prompt('Qual seu nome?')
+  var idade = prompt(`Olá ${nome}, Quantos anos você têm?`)
+  alert(`Acabo de conhecer ${nome}, que têm ${idade} anos de idade. É um grande Prazer!`)
+}
