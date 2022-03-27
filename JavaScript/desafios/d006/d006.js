22eeaf8f5f548fef35e7ce2042fc4e3b75f09c34
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-03-27 17:24:40 -0300

    finalizado desafio 006

--- JavaScript/desafios/d006/d006.js
+++ JavaScript/desafios/d006/d006.js
@@ -1,9 +1,9 @@
 function calcule(){
-  var c = prompt('Digite uma temperatura  em C (Celsius)')
-  var k = (c+273.15)
-  var f = ((c*1.8)+32)
-  var cont = document.getElementById('container')
-  cont.innerHTML(`<h2>A temperatura  de ${c}C, corresponde a...</h2>
-  <p>${k} k (Kelvin)</p>
-  <p>${f} F (Fahrenheit)</p>`)
+  var cel = prompt('Digite uma temperatura  em C (Celsius)')
+  var res = document.getElementById('container')
+  var kel = (cel + 273.15)
+  var fah = ((cel * 1.8) + 32)
+  res.innerHTML(`<h2>A temperatura  de ${cel} C, corresponde a...</h2>
+  <p>${kel} k (Kelvin)</p>
+  <p>${fah} F (Fahrenheit)</p>`)
 }
