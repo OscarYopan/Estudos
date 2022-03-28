76ade9e8c8ea37b9e7ee7e3dc77f86c04ba6fd85
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-03-28 17:45:29 -0300

    iniciei o desafio 07

---
+++ JavaScript/desafios/d007/d007.js
@@ -0,0 +1,6 @@
+var cot = prompt('Qual a cotação atdual do Dólar?')
+function calcule(){
+  var cart = prompt('Quantos Reais(R$) você têm na carteira?')
+  var cont = window.document.querySelector('div.container')
+  var conv = (cart / cot)
+}
