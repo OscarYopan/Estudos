8bc0646bdf5cb41981ea50e1f43b09edf3dd41dc
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-03-28 23:17:56 -0300

    finalizado desafio 07

--- JavaScript/desafios/d007/d007.js
+++ JavaScript/desafios/d007/d007.js
@@ -1,6 +1,7 @@
 var cot = prompt('Qual a cotação atdual do Dólar?')
 function calcule(){
   var cart = prompt('Quantos Reais(R$) você têm na carteira?')
-  var cont = window.document.querySelector('div.container')
+  var cont = document.getElementById('container')
   var conv = (cart / cot)
+  cont.innerHTML =`<p>Com os R$ ${cart} reais, você consegue comprar U$$ ${conv} doláres.</p>`
 }
