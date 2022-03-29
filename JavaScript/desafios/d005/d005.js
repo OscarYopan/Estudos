3189b825d719d66db4f6334c283c868338143002
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-03-28 23:20:50 -0300

    corrigido desafio 005

--- JavaScript/desafios/d005/d005.js
+++ JavaScript/desafios/d005/d005.js
@@ -1,17 +1,17 @@
 function calcule(){
   var m = prompt('Digite uma distância em metros(m)')
   var cont = document.getElementById('container')
-  var km = (m/100)
+  var km = (m/1000)
   var hm = (m/100)
   var dam = (m/10)
   var dm = (m*10)
   var cm = (m*100)
   var mm = (m*1000)
-  cont.innerHTML(`<h2>A distância de ${m} metros, corresponde a...</h2>
+  cont.innerHTML=`<h2>A distância de ${m} metros, corresponde a...</h2>
   <p>${km} quilômetros (km)</p>
   <p>${hm} hectômetros (Hm)</p>
   <p>${dam} decâmetros (Dam)</p>
   <p>${dm} decimetros (dm)</p>
   <p>${cm} centímetros (cm)</p>
-  <p>${mm} milímetros (mm)</p>`)
+  <p>${mm} milímetros (mm)</p>`
 }
