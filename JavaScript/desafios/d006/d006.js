267615fa61168fd3966f844a7e2f4217d00e12a2
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-03-28 23:19:14 -0300

    corrigido desafio 06

--- JavaScript/desafios/d006/d006.js
+++ JavaScript/desafios/d006/d006.js
@@ -3,7 +3,7 @@ function calcule(){
   var res = document.getElementById('container')
   var kel = (cel + 273.15)
   var fah = ((cel * 1.8) + 32)
-  res.innerHTML(`<h2>A temperatura  de ${cel} C, corresponde a...</h2>
+  res.innerHTML=`<h2>A temperatura  de ${cel} C, corresponde a...</h2>
   <p>${kel} k (Kelvin)</p>
-  <p>${fah} F (Fahrenheit)</p>`)
+  <p>${fah} F (Fahrenheit)</p>`
 }
