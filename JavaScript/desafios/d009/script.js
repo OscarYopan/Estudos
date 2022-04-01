21da4746c796bca4132b9bf74912546eb755c829
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-03-31 23:54:59 -0300

    finalizado

--- JavaScript/desafios/d009/script.js
+++ JavaScript/desafios/d009/script.js
@@ -1,3 +1,11 @@
 function calculo(){
-  
+  var name = document.getElementById('nome')
+  var salat = document.getElementById('salario')
+  var aumento = document.getElementById('aumento')
+  var res = document.getElementById('res')
+  var salatual = (salat.value)
+  var aument = (aumento.value) 
+  var salnov = (salatual + (salatual/aument))
+  var nome = (name.value)
+  res.innerHTML = `<p>${nome} seu salário foi aumentado para ${salnov}</p>`
 }
