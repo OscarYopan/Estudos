58ff4adb57e3ea348580cc548f103d28b8f854f2
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-04-02 19:58:41 -0300

    Aula 14 ex

---
+++ JavaScript/exercicios/aula14/ambiente.js
@@ -0,0 +1,9 @@
+for(c=1; c<=5; c++){
+  console.log(`Contagem: ${c}`)
+}
+for(c=0; c<=100; c = c + 10){
+  console.log(`Contagem: ${c}`)
+}
+for(c=1000; c>=0; c = c -100){
+  console.log(`Regressiva: ${c}`)
+}
