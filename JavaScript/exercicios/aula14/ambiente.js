aeae4abf5959352a9136ef6d3da6ac7ebfec0b8d
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-04-02 20:09:20 -0300

    melhorias

--- JavaScript/exercicios/aula14/ambiente.js
+++ JavaScript/exercicios/aula14/ambiente.js
@@ -1,9 +1,15 @@
+console.log('Primeira Contagem')
 for(c=1; c<=5; c++){
   console.log(`Contagem: ${c}`)
 }
+console.log('Fim da Primeira Contagem')
+console.log('Segunda Contagem')
 for(c=0; c<=100; c = c + 10){
   console.log(`Contagem: ${c}`)
 }
+console.log('Fim da Segunda Contagem')
+console.log('Terceira Contagem')
 for(c=1000; c>=0; c = c -100){
   console.log(`Regressiva: ${c}`)
 }
+console.log('Fim da Terceira Contagem')
