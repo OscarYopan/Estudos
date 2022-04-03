703f15c032a32ed47c4543ef7169723be56c39fc
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-04-02 21:00:14 -0300

    desenvovendo ex016

---
+++ JavaScript/exercicios/aula14/ex016/script/ex016.js
@@ -0,0 +1,7 @@
+function contar(){
+  var incio = document.getElementById('incio')
+  var fim = document.getElementById('fim')
+  var passo = document.getElementById('passo')
+  var res = document.getElementById('saida')
+  res.innerHTML = `<p>teste</p>`
+}
