5b587e5b4c676213b922c9db023a848f5c1e2e10
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-04-02 22:37:36 -0300

    criado ex017 JS

--- JavaScript/exercicios/aula14/ex016/script/ex016.js
+++ JavaScript/exercicios/aula14/ex016/script/ex016.js
@@ -1,7 +1,28 @@
 function contar(){
-  var incio = document.getElementById('incio')
-  var fim = document.getElementById('fim')
-  var passo = document.getElementById('passo')
-  var res = document.getElementById('saida')
-  res.innerHTML = `<p>teste</p>`
+  let inicio = document.getElementById('txti')
+  let fim = document.getElementById('txtf')
+  let passo = document.getElementById('txtp')
+  let res = document.getElementById('saida')
+  let i = Number(inicio.value)
+  let f = Number(fim.value)
+  let p = Number(passo.value)
+  //caso falte preencher algum dado
+  if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length ==0){
+    alert('[Erro] Preencha todos os campor')
+    res.innerHTML=`<p>[Erro] Impossivel Contar.</p>`
+  }else if(i<f){
+    //contagem Crescente
+    while(i<f){
+      res.innerHTML+=`${i} \u{27A1}`
+      i = i + p
+    }
+    res.innerHTML+=`\u{1f3c1}`    
+  }else if(i>f){
+    //contagem regressiva
+    while(i>f){
+      res.innerHTML+=`${i} \u{27A1}`
+      i = i - p
+    }
+    res.innerHTML+=`\u{1f3c1}`
+  }
 }
