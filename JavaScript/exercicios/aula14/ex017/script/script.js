d859cf1dd75d12ddf650daff7678a53b0ff68042
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-04-02 23:50:07 -0300

    Concluído

--- JavaScript/exercicios/aula14/ex017/script/script.js
+++ JavaScript/exercicios/aula14/ex017/script/script.js
@@ -1,16 +1,17 @@
 function calcular(){
   var num = document.getElementById('txtn')
   var n = Number(num.value)
-  var saida = document.getElementById('saida')
   c = 1
-  //saida.innerHTML=`<p>teste ${n}</p>`
   if(n > 0 && n < 11){
     while(c<11){
+      var saida = document.getElementById('saida')
       var r = c * n
-      saida.innerHTML+=`${r}`
+      saida.innerHTML+=`<p>${c} x ${n} = ${r}</p>`
       c++
     }
   }else{
+    var saida = document.getElementById('saida')
+    alert('[Erro] Numero invalido.')
     saida.innerHTML=`<p>[Erro] Numero invalido, escolha um numero entre 1 e 10</p>`
   }
 }
