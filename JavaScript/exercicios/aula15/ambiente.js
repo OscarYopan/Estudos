2fa529c6ffadd149794498125149081c7b79738e
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-04-04 16:46:15 -0300

    test array

--- JavaScript/exercicios/aula15/ambiente.js
+++ JavaScript/exercicios/aula15/ambiente.js
@@ -1,2 +1,8 @@
-let num = []
+let num = [9,8,7,6,5,4,3,2,1,0]
 console.log(`Nosso vetor é: ${num}`)
+//num[10] = 10//adicionar um elemteno em uma posção do vetor
+//num.push(11)//adiciona um elemento no final do vetor
+//console.log(num)
+console.log(`O comprimento do vetor é: ${num.length}`/*comando mede o copmprimento de um vetor*/)
+//console.log(num.sort()/*Coloca os atributos de um vetor em ordem*/)
+console.log(`O sexto elemento é: ${num[6]}`)
