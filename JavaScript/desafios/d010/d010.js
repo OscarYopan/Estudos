ab122a6c601ee8f36c49cef2e2dad31d1ffbf0f3
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-04-05 17:26:11 -0300

    Update d010.js

--- JavaScript/desafios/d010/d010.js
+++ JavaScript/desafios/d010/d010.js
@@ -3,9 +3,9 @@ function calcular(){
   var bb = prompt('Qual é o valor de "b"?')
   var cc = prompt('Qual é o valor de "c"?')
   var res = document.getElementById('res')
-  var a = Number(aa.value)
-  var b = Number(bb.value)
-  var c = Number(cc.value)
+  var a = Number(aa)
+  var b = Number(bb)
+  var c = Number(cc)
   var delta = (a + b + c)
   res.innerHTML = `<p>O resultado é: ${delta}</p>`
 }
