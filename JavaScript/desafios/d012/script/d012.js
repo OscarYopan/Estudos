9ca6758c5d8622760df2bb2a97259692c0bd229b
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-04-06 21:45:18 -0300

    Concluido desafio 012

--- JavaScript/desafios/d012/script/d012.js
+++ JavaScript/desafios/d012/script/d012.js
@@ -6,7 +6,9 @@ function verificar(){
   let at = Number(atual.value).toFixed(2)
   let dif = (ant - at).toFixed(2)
   let aum = (at - ant).toFixed(2)
-  let porpos = ((ant/100)*dif)
+  let por = ((at*100)/ant)
+  let porpos = (100-por).toFixed(1)
+  let porneg = (por-100).toFixed(1)
   res.innerHTML=`<p><strong>Analisando os valores informados:</strong></p>
   <p>O produto custava ${ant} e agora custa ${at}.</p>`
   if(at < ant){
@@ -15,8 +17,8 @@ function verificar(){
     res.innerHTML+=`<p>Uma variação de ${porpos}% para baixo.</p>`
   }else if(at > ant){
     res.innerHTML+=`<p>Agora o produto está mais caro.</p>`
-    res.innerHTML+=`<p>O preço aumento R$ ${aum}, em relação ao preço inicial.</p>`
-    res.innerHTML+=`<p>Uma variação de ${at}% para baixo.</p>`
+    res.innerHTML+=`<p>O preço aumentou R$ ${aum}, em relação ao preço inicial.</p>`
+    res.innerHTML+=`<p>Uma variação de ${porneg}% para cima.</p>`
   }else if(at == ant && dif == 0){
     res.innerHTML+=`<p>O produto está com o mesmo preço de antes.</p>`
     res.innerHTML+=`<p>Não há variação no preço.</p>`
