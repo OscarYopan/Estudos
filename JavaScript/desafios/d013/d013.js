b98612f2a0ddcf6028f75d0718beaf3eacfa1f2d
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-04-06 22:59:04 -0300

    Criado JS do d013

---
+++ JavaScript/desafios/d013/d013.js
@@ -0,0 +1,26 @@
+function media(){
+  let nome = document.getElementById('nome')
+  let nota1 = document.getElementById('nota1')
+  let nota2 = document.getElementById('nota2')
+  let name = (nome.value)
+  let n1 = Number(nota1.value).toFixed(1)
+  let n2 = Number(nota2.value).toFixed(1)
+  let res = document.getElementById('res')
+  let media = ((n1/2)+(n2/2)).toFixed(1)
+  if (media<3 && media>=0){
+    res.innerHTML=`<p><strong>Aluno ${name} aproveitamento:</strong></p>
+    <p>Com as notas ${n1} e ${n2} sua média foi: ${media}.</p>
+    <p>O Aluno está <strong>REPROVADO!</strong></p>`
+  }else if(media>= 3 && media<6){
+    res.innerHTML=`<p><strong>Aluno ${name} aproveitamento:</strong></p>
+    <p>Com as notas ${n1} e ${n2} sua média foi: ${media}.</p>
+    <p>O Aluno está de <strong>RECUPERAÇÂO!</strong></p>`
+  }else if(media>=6 && media<=10){
+    res.innerHTML=`<p><strong>Aluno ${name} aproveitamento:</strong></p>
+    <p>Com as notas ${n1} e ${n2} sua média foi: ${media}.</p>
+    <p>O Aluno está <strong>APROVADO!</strong></p>`    
+  }else if(media>10){
+    alert('[Erro] Nota(s) inválida(s)!')
+    res.innerHTML=`<p>[Erro] Favor revise as notas, e tente novamente.</p>`
+  }
+}
