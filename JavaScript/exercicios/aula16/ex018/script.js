85511dd97fa4e1d6121a2bfeb54a180308082cf3
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-04-08 22:51:56 -0300

    desenvolvendo

--- JavaScript/exercicios/aula16/ex018/script.js
+++ JavaScript/exercicios/aula16/ex018/script.js
@@ -1,25 +1,28 @@
-let num = document.getElementById('novonumero')
-let lista = document.getElementById('nlista')
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
 let res = document.querySelector('section#res')
 let valores = []
 
 function isnumero(n){
-  if(Number(n) > 0 && Number(n) < 101){
-    return true
-  }else{
-    return false
-  }
-}
-function inlista(n, l){
-  if(l.indexof(Number(n) !=-1)){
+  if(Number(n) >= 1 && Number(n) <= 100){
     return true
   }else{
     return false
   }
 }
 
-function numeros(){
-  if(isnumero(num.value) && !inlista(num.value, valores)){
+/*
+function inlista(n, l){
+  if(l.indexof(Number(n)) != -1){
+    return true
+  }else{
+    return false
+  }
+}
+*/
+
+function adicionar(){
+  if(isnumero(num.value) /*&& !inlista(num.value, valores)*/){
     valores.push(Number(num.value))
     let item = document.createElement('option')
     item.text=`Valor ${num.value} adicionado.`
@@ -27,9 +30,10 @@ function numeros(){
   }else{
     alert('[Erro] Numero inválido ou repetido')
   }
+  num.value = ``
+  num.focus()
 }
 
-/*function finalizar(){
+function finalizar(){
   alert('Tchau')
 }
-*/
