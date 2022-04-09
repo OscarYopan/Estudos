e39a26ee6634aa6e7f73108004a132d8d34f4101
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-04-09 16:42:26 -0300

    Update d012.js

--- JavaScript/desafios/d012/script/d012.js
+++ JavaScript/desafios/d012/script/d012.js
@@ -13,11 +13,11 @@ function verificar(){
   <p>O produto custava ${ant} e agora custa ${at}.</p>`
   if(at < ant){
     res.innerHTML+=`<p>Agora o produto está mais barato.</p>`
-    res.innerHTML+=`<p>O preço caiu R$ ${dif}, em relação ao preço inicial.</p>`
-    res.innerHTML+=`<p>Uma variação de ${porpos}% para baixo.</p>`
+    res.innerHTML+=`<p>O preço aumentou R$ ${-1*(dif)}, em relação ao preço inicial.</p>`
+    res.innerHTML+=`<p>Uma variação de ${porneg}% para cima.</p>`
   }else if(at > ant){
     res.innerHTML+=`<p>Agora o produto está mais caro.</p>`
-    res.innerHTML+=`<p>O preço aumentou R$ ${aum}, em relação ao preço inicial.</p>`
+    res.innerHTML+=`<p>O preço diminuiu R$ ${aum}, em relação ao preço inicial.</p>`
     res.innerHTML+=`<p>Uma variação de ${porneg}% para cima.</p>`
   }else if(at == ant && dif == 0){
     res.innerHTML+=`<p>O produto está com o mesmo preço de antes.</p>`
