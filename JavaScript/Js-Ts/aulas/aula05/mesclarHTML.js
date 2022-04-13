f34b4533ec8d010f12503499f72d205adcfd9c8d
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-04-13 15:29:41 -0300

    mesclar

---
+++ JavaScript/Js-Ts/aulas/aula05/mesclarHTML.js
@@ -0,0 +1,9 @@
+console.log('teste!')
+function teste(){
+  var valor1 = prompt('Primeiro valor')
+  var valor2 = prompt('Segundo valor')
+  var v1 = Number(valor1)
+  var v2 = Number(valor2)
+  var soma = v1 + v2
+  console.log(`A soma dos valores é: ${soma}`)
+}
