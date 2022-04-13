428e94a0fe157f1f666ae3a210eaddf7725b045d
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-04-13 15:00:28 -0300

    comentarios em JS

---
+++ JavaScript/Js-Ts/aulas/aula04/comentarios.js
@@ -0,0 +1,16 @@
+// escrever comentario de uma linha
+
+/*
+  comentario de multiplas linhas ou bloco
+  códigos dentro de comentario não são executados
+*/
+
+console.log(1)
+console.log(2)
+//console.log(3)
+console.log(4)
+/*
+console.log(5)
+console.log(6)
+*/
+console.log(7)
