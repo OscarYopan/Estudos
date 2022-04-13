a38f061c035bfbb48175a7a33dc216d6c4a5fa17
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-04-13 16:44:59 -0300

    const js

---
+++ JavaScript/Js-Ts/aulas/aula07/constantes.js
@@ -0,0 +1,15 @@
+/*
+  Constantes
+  - Não se deve usar palavras reservadas (if, else, let ...);
+  - Recomenda-se usar nomes significativos;
+  - Não pode começar o nome da constante com um numero;
+  - Recomenda-se usar letras minusculas no começo;
+  - não deve conter 'espaços' ou 'traços';
+  - Caso nome composto use camelCase;
+  - Constantes são case sensetive (usa-se escrita totalmente igual);
+  - Não se pode mudar o valor de uma constante;
+  - Sempre que declarada já deve ser definido o valor;
+  - valor da constante é imutavel.
+*/
+const nome = 'João'
+console.log(nome)
