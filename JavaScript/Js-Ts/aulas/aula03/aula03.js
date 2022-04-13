3205933a0858c9507467d4c394cfc71186c45506
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-04-13 14:45:12 -0300

    varios

---
+++ JavaScript/Js-Ts/aulas/aula03/aula03.js
@@ -0,0 +1,6 @@
+let  o = ('Olá')
+console.log('Olá "Mundo!"');
+console.log("Olá 'Mundo!'");
+console.log(`${o} Mundo!`);
+console.log(0123456789);
+console.log(15, 25.5, 'Olá Mundo!'); // (Number, Number, string)
