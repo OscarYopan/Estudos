4ae8915b854c1b0d175c5e618f70f10de7ae1a1d
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-04-15 21:38:11 -0300

    Create aula09.js

---
+++ JavaScript/Js-Ts/aulas/aula09/aula09.js
@@ -0,0 +1,9 @@
+// Diferenças entre 'var' e 'let'
+// ECMAscript 2015 (ES6)
+var nome = 'Oscar';
+var nome = 'Luis'; // pode redeclarar o variavel com 'var'
+let name = 'Oscar'
+/*let name = 'Luis' */ // não se pode redeclar variavel com 'let'
+name = 'Luis' // para alterar o calor apenas coloca o nome da variavel
+console.log(nome)
+console.log(name)
