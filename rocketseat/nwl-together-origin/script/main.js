05da5286ee94266b4095a54aa9c25f38c9828666
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-04-21 16:35:18 -0300

    services, scroll menu

--- rocketseat/nwl-together-origin/script/main.js
+++ rocketseat/nwl-together-origin/script/main.js
@@ -1,4 +1,4 @@
-//-------------Abre e Fecha Menu------------------------
+//-------------------Abre e Fecha Menu------------------------
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
@@ -7,10 +7,21 @@ for(const element of toggle){
     nav.classList.toggle('show')
   })
 }
-//------------ Ao clicar esconder o Menu ----------------
+//-------------- Ao clicar esconder o Menu --------------------
 const links = document.querySelectorAll('nav ul li a')
 for(const link of links){
   link.addEventListener('click', function(){
     nav.classList.remove('show')
   })
 }
+//---------------Sombra Header quando Scroll -----------------
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function(){
+  if(window.scrollY >= navHeight){
+    header.classList.add('scroll')
+  }else{
+    header.classList.remove('scroll')
+  }
+})
