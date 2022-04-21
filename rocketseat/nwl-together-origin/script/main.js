1948c2328478c7d4fd2521336aa2ad93ae9e7ab4
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-04-21 00:30:18 -0300

    about

--- rocketseat/nwl-together-origin/script/main.js
+++ rocketseat/nwl-together-origin/script/main.js
@@ -1,8 +1,16 @@
+//-------------Abre e Fecha Menu------------------------
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for(const element of toggle){
   element.addEventListener('click', function(){
     nav.classList.toggle('show')
+  })
+}
+//------------ Ao clicar esconder o Menu ----------------
+const links = document.querySelectorAll('nav ul li a')
+for(const link of links){
+  link.addEventListener('click', function(){
+    nav.classList.remove('show')
   })
 }
