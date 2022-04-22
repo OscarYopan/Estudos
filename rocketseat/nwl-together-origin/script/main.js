4c03b2584072691668edfacd332fe7ef30778611
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-04-22 14:41:05 -0300

    scroll reaveal

--- rocketseat/nwl-together-origin/script/main.js
+++ rocketseat/nwl-together-origin/script/main.js
@@ -40,3 +40,19 @@ const swiper = new Swiper('.swiper-container', {
     }
   }
 })
+//-------------------- Scroll Reveal------------------------
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `, { interval: 100 }
+)
