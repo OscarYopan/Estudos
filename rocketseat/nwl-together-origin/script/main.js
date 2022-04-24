514c6dd361136baf2f89a1611575e68865cbdcd1
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-04-24 20:05:48 -0300

    version 1 finalization

--- rocketseat/nwl-together-origin/script/main.js
+++ rocketseat/nwl-together-origin/script/main.js
@@ -1,31 +1,37 @@
-//-------------------Abre e Fecha Menu------------------------
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
-for(const element of toggle){
-  element.addEventListener('click', function(){
+for (const element of toggle) {
+  element.addEventListener('click', function () {
     nav.classList.toggle('show')
   })
 }
-//-------------- Ao clicar esconder o Menu --------------------
+
+/* quando clicar em um item do menu, esconder o menu */
 const links = document.querySelectorAll('nav ul li a')
-for(const link of links){
-  link.addEventListener('click', function(){
+
+for (const link of links) {
+  link.addEventListener('click', function () {
     nav.classList.remove('show')
   })
 }
-//---------------Sombra Header quando Scroll -----------------
+
+/* mudar o header da página quando der scroll */
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function(){
-  if(window.scrollY >= navHeight){
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
     header.classList.add('scroll')
-  }else{
+  } else {
+    // menor que a altura do header
     header.classList.remove('scroll')
   }
-})
-//-----------------------Swiper/Slider ------------------------
+}
+
+/* Testimonials carousel slider swiper */
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   pagination: {
@@ -40,13 +46,15 @@ const swiper = new Swiper('.swiper-container', {
     }
   }
 })
-//-------------------- Scroll Reveal------------------------
+
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
 const scrollReveal = ScrollReveal({
   origin: 'top',
   distance: '30px',
   duration: 700,
   reset: true
 })
+
 scrollReveal.reveal(
   `#home .image, #home .text,
   #about .image, #about .text,
@@ -54,16 +62,49 @@ scrollReveal.reveal(
   #testimonials header, #testimonials .testimonials
   #contact .text, #contact .links,
   footer .brand, footer .social
-  `, { interval: 100 }
+  `,
+  { interval: 100 }
 )
 
-//--------------To Top Button------------------------------
+/* Botão voltar para o topo */
+const backToTopButton = document.querySelector('.to-top')
 
-const toTopButton = document.querySelector('.to-top')
-window.addEventListener('scroll', function(){
-  if(window.scrollY >= 400){
-    toTopButton.classList.add('show')
-  }else{
-    toTopButton.classList.remove('show')
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
   }
+}
+
+/* Menu ativo conforme a seção visível na página */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
 })
