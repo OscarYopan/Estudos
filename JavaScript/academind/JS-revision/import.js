bd6ca49f718a078f4b92861b931a3cbffd3f7393
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-05 23:14:27 -0300

    import exemplo

---
+++ JavaScript/academind/JS-revision/import.js
@@ -0,0 +1,11 @@
+/* ----------- DEFAULT EXPORT/IMPORT ----------------------*/
+// importa o default definido no export, mesmo com o nome difente
+import name from './export'
+import prs from './export'
+
+/*------------ Named EXPORT/IMPORT ------------------------*/
+// use exatamente o mesmo nome na importação do nome definido na exportação.
+
+import { baseData } from './export2'
+import { clean } from './export2'
+
