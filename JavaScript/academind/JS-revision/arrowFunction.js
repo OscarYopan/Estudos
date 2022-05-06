5484777c4ac2d393cc7a9e51db25d9c041bd3bc7
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-05 22:52:00 -0300

    arrow function exemple

---
+++ JavaScript/academind/JS-revision/arrowFunction.js
@@ -0,0 +1,15 @@
+//Normal funciotn
+
+/*
+const multiply = (number) => {
+  return number * 2
+}
+console.log(multiply(2));
+*/
+
+
+//Arrow Funcion
+
+const multiply = (number) => number * 2
+
+console.log(multiply(2))
