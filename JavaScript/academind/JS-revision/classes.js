f275242a57e4cad7218849ac9cd5847bef3b319e
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-08 20:05:54 -0300

    classes

---
+++ JavaScript/academind/JS-revision/classes.js
@@ -0,0 +1,7 @@
+// Classes são o que também chamamos de objetos
+
+class Person{
+  name = 'max' //podem ter propriedades
+  call = () => {'...'} // podem ter também métodos (function, arrow functions etc.)
+}
+
