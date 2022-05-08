18b20e5d7c9dd66b4a05bf2cb5eccdc2d151a262
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-08 20:23:40 -0300

    Update classes.js

--- JavaScript/academind/JS-revision/classes.js
+++ JavaScript/academind/JS-revision/classes.js
@@ -1,7 +1,28 @@
 // Classes são o que também chamamos de objetos
-
+/*
 class Person{
   name = 'max' //podem ter propriedades
   call = () => {'...'} // podem ter também métodos (function, arrow functions etc.)
 }
 
+//USAGE
+
+const myPerson = new Person()
+
+myPerson.call()
+
+console.log(myPerson.name)
+
+//inheritance
+
+class Person extends Master
+*/
+
+class Person{
+  constructor(){
+    this.name = 'max'
+ }
+  printMyName(){
+    console.log(this.name);
+  }
+}
