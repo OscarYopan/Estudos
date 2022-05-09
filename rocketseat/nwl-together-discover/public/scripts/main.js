a5bafd30c80dd0ea558989e10e22c5028d2d9f15
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-09 12:40:05 -0300

    Update main.js

--- rocketseat/nwl-together-discover/public/scripts/main.js
+++ rocketseat/nwl-together-discover/public/scripts/main.js
@@ -2,12 +2,21 @@ import Modal from './modal.js';
 
 const modal = Modal()
 
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
 //Ler todos os botões com a class 'check'
 const checkButtons = document.querySelectorAll(".actions a.check")
 
 checkButtons.forEach(button => {
     //adicionar a escuta
     button.addEventListener("click", event => {
+
+        modalTitle.innerHTML = 'Marcar como lida'
+        modalDescription.innerHTML = 'Tem certeza que quer marcar esta pergunta como lida?'
+        modalButton.innerHTML = 'Sim, marcar'
+
         //abrir modal
         modal.open()
     })
@@ -19,6 +28,10 @@ const deleteButton = document.querySelectorAll('.actions a.delete')
 
 deleteButton.forEach(button => {
     button.addEventListener('click', event =>{
+
+        modalTitle.innerHTML = 'Excluir pergunta'
+        modalDescription.innerHTML = 'Tem certeza que deseja excluir esta pergunta?'
+
         //fechar modal
         modal.open()
     } )
