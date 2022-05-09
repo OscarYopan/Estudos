e2c521874fd3ad55598b9f2f04b55c6887dc4465
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-09 11:22:07 -0300

    criando js modal

---
+++ rocketseat/nwl-together-discover/public/scripts/main.js
@@ -0,0 +1,12 @@
+import { Modal } from "./modal";
+
+const modal = Modal()
+
+//Ler todos os botões com a class 'check'
+
+//Ler quando o 'marcar como lido' for clicado
+
+addEventListener
+
+//abrir a modal
+modal.open()
---
+++ rocketseat/nwl-together-discover/public/scripts/modal.js
@@ -0,0 +1,13 @@
+module.exports = function Modal(){
+    function open(){
+        //funcionalidade de adicionar a class 'active' do modal
+    }
+    function close(){
+        //funcionalidade de remover a class 'active' do modal
+    }
+
+    return{
+        open,
+        close
+    }
+}
