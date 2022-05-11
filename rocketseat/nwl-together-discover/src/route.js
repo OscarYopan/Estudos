20cf02b648a9bb994cf29298d04fd4644e55d89c
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-10 21:46:03 -0300

    -

--- rocketseat/nwl-together-discover/src/route.js
+++ rocketseat/nwl-together-discover/src/route.js
@@ -8,4 +8,7 @@ route.get('/', (req, res) => res.render('index'))
 route.get('/room', (req, res) => res.render('room'))
 route.get('/create-pass', (req, res) => res.render('create-pass'))
 
+//Formato de informação que o modal deve passar
+//route.post('/room/:room/:question/:action')
+
 module.exports = route
