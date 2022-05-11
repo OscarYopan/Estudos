e2b86946ac6473a9e5b414e8aa2aca2eceab719c
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-10 22:12:05 -0300

    -

--- rocketseat/nwl-together-discover/public/scripts/main.js
+++ rocketseat/nwl-together-discover/public/scripts/main.js
@@ -25,6 +25,14 @@ deleteButton.forEach(button => {
 
 function handleClick(event, check = true){
     event.preventDefault()
+
+    const slug = check ? 'check': 'delete'
+    const roomId = document.querySelector('#room-id').dataset.id
+    const questionId = event.target.dataset.id
+
+    const form = document.querySelector('.modal form')
+    form.setAttribute('action', `/room/${roomId}/${questionId}/${slug}`)
+    
     modalTitle.innerHTML = check ? 'Marcar como lida' : 'Excluir pergunta'
     modalDescription.innerHTML = check ? 'Tem certeza que deseja marcar como lida esta pergunta?' : 'Tem certeza que deseja excluir esta pergunta?'
     modalButton.innerHTML = check ? 'Sim, marcar' : 'Sim, excluir'
