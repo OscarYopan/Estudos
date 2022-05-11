25dc47a3d6f13eb3e718c7e3d00f22232af6af0e
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-10 22:52:56 -0300

    -

--- rocketseat/nwl-together-discover/src/route.js
+++ rocketseat/nwl-together-discover/src/route.js
@@ -6,9 +6,10 @@ const res = require('express/lib/response')
 
 const route = express.Router()
 
-route.get('/', (req, res) => res.render('index'))
+route.get('/', (req, res) => res.render('index', {page: 'enter-room'}))
+route.get('/create-pass', (req, res) => res.render('index', {page: 'create-pass'}))
+
 route.get('/room', (req, res) => res.render('room'))
-route.get('/create-pass', (req, res) => res.render('create-pass'))
 
 //Formato de informação que o modal deve passar
 route.post('/room/:room/:question/:action', QuestionController.index)
