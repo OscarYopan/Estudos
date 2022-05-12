1e8a78e89728526b19f73f720d59fbe14c884ca4
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-12 12:08:12 -0300

    database config

---
+++ rocketseat/nwl-together-discover/src/db/init.js
@@ -0,0 +1,22 @@
+const { init } = require('express/lib/application')
+const Database = require('./config')
+
+const initDb = {
+  async init(){
+    const db = await Database()
+
+    await db.exec(`CREATE TABLE rooms (
+      id INTEGER PRIMARY KEY,
+      pass TEXT
+      )`)
+
+    await db.exec(`CREATE TABLE questions (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      titulo TEXT,
+      check INT
+    )`)
+    await db.close()
+  }
+}
+
+initDb.init()
