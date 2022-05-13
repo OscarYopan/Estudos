0149a0862ee5d8ea06a9bd6d204be2d5c6cb212d
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-13 16:13:43 -0300

    ID no Style

--- rocketseat/nwl-together-discover/src/route.js
+++ rocketseat/nwl-together-discover/src/route.js
@@ -10,7 +10,7 @@ const route = express.Router()
 route.get('/', (req, res) => res.render('index', {page: 'enter-room'}))
 route.get('/create-pass', (req, res) => res.render('index', {page: 'create-pass'}))
 
-route.get('/room/:room', (req, res) => res.render('room'))
+route.get('/room/:room', RoomController.open)
 
 //Formato de informação que o modal deve passar
 route.post('/question/:room/:question/:action', QuestionController.index)
