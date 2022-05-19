7481e3148e98f7ac09f6308c8516d1a7ae2075bf
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-18 21:37:18 -0300

    operadores aritimeticos

---
+++ JavaScript/Js-Ts/aulas/aula11/operadoresAritimeticos.js
@@ -0,0 +1,31 @@
+/*     Operadores Aritiméticos   */
+
+//Adição
+const num1 = 5
+const num2 = 10
+console.log(num1 + num2);
+
+//Concatenação - pois a const num3 é uma string
+const num3 = '2'
+const num4 = 4
+console.log(num3 + num4);
+
+//Subtração
+const num5 = 5
+const num6 = 10
+console.log(num5 - num6);
+
+//Multiplicação
+const num7 = 5
+const num8 = 10
+console.log(num7 * num8);
+
+//Divisão
+const num9 = 5
+const num10 = 10
+console.log(num9 / num10);
+
+//Potenciação
+const num11 = 5
+const num12 = 10
+console.log(num11 ** num12);
