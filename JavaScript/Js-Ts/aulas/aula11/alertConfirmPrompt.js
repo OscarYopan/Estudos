75a27b295b835d9ca541b52150238afd8eb7f0e3
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-19 17:34:23 -0300

    alert, confirm e prompt

---
+++ JavaScript/Js-Ts/aulas/aula11/alertConfirmPrompt.js
@@ -0,0 +1,10 @@
+// Alert - Apenas mostra uma mensagem na tela - Retorna nada para a memoria 'Undefined'
+alert('Professor Girafales!')
+
+// Confirm - Retorna um valor Boolean 'true' ou 'false'
+const confirma = confirm('Gostaria de Uma Chucara de café?')
+alert(confirma)
+
+// Prompat - Retorna 'String' digitado pelo usuario
+const encomodo = prompt('Não seria muito encomodo?')
+alert(encomodo)
