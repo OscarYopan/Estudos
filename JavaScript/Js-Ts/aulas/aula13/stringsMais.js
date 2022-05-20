198638603a89f81e56063d8f3efa6e88f968c93b
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-19 22:35:21 -0300

    Create stringsMais.js

---
+++ JavaScript/Js-Ts/aulas/aula13/stringsMais.js
@@ -0,0 +1,15 @@
+//Strings
+
+let umaString = 'um "texto"' //aspas dentro de aspas
+
+console.log(umaString);
+
+let outraString = "um \"texto\"" // pode colocar assim tbm
+console.log(outraString)
+
+//                01234567    => indice da string
+let moreString = 'um texto' //aspas dentro de aspas
+console.log(moreString[4]); //vai mostrar o caractere do indice solicitado
+
+let anotherString = 'um texto'
+console.log(anotherString.length); //adiciona-se '.'  para acionar funcões para a string
