d92991c2bbc5eec479715081a1fd0eea38792856
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-20 16:10:54 -0300

    Create objetoMath.js

---
+++ JavaScript/Js-Ts/aulas/aula15/objetoMath.js
@@ -0,0 +1,18 @@
+// Math. é um objeto que nos trás muitas possibilidades
+
+//let numero1 = 9.48785489
+//let numero2 = Math.floor(numero1) // arredonda para baixo
+//let numero2 = Math.ceil(numero1) // arredonda para cima
+//let numero2 = Math.round(numero1) // arredonda para o numero mais proximo
+//console.log(numero2);
+
+//console.log(Math.max(2, 78, 98, 54, 12, -87, -65, 1500, 1)); //mostrar o maior numero da sequencia
+//console.log(Math.min(2, 78, 98, 54, 12, -87, -65, 1500, 1)); //mostrar o menor numero da sequencia
+
+//const aleatorio = Math.random() *  (10 - 5) + 5 // gera um numero aleatório
+//console.log(aleatorio);
+
+//console.log(Math.PI); // mostra o pi
+
+//let num1 = 9
+//console.log(num1 ** (1/2)); // Raiz quadrada
