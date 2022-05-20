b2ad5ec4826af49088f73a65230ee1090eed938e
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-20 15:21:36 -0300

    Create numerosMais.js

---
+++ JavaScript/Js-Ts/aulas/aula14/numerosMais.js
@@ -0,0 +1,17 @@
+let numero1 = 1 //typeof Number
+let numero2 = 2.5 //typeof Number
+let numero3 = 10.1587924 //typeof Number
+let numero4 = 10
+
+//.toString converte  de Number para string mantem o typeof da sua variavel como number
+console.log(numero1.toString() + numero2);
+
+//.tostring na variavel para mudar o typeof de number para string
+numero1 = numero1.toString()
+console.log(typeof numero1)
+
+// Mostrar apenas algumas casas apos o ponto
+console.log(numero3.toFixed(2));
+
+// Verificar se é um número inteiro
+console.log(Number.isInteger(numero4));
