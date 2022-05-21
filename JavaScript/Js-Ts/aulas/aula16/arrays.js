8e38ad3ca437280c1a29b6145d4193006ae95444
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-21 19:12:32 -0300

    Create arrays.js

---
+++ JavaScript/Js-Ts/aulas/aula16/arrays.js
@@ -0,0 +1,16 @@
+//ARRAYS
+/*
+- Pode-se colocar qualquer coisa dentro de uma array
+- Como pratica se coloca apenas conteudos do mesmo tipó dentro de uma array
+*/ 
+//                0         1         2         3         4
+const alunos = ['Oscar', 'Amanda', 'Vitor', 'Alicia', 'Sophia']
+console.log(alunos);
+console.log(alunos[3]);
+
+const pontuacao = [7, 8, 5, 6, 9]
+console.log(pontuacao);
+console.log(pontuacao[3]);
+
+pontuacao[3] = 7
+console.log(pontuacao[3]);
