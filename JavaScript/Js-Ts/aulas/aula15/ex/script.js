74b77149921df26e281c8c7a00f525090c81ca3e
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-21 13:53:38 -0300

    Update script.js

--- JavaScript/Js-Ts/aulas/aula15/ex/script.js
+++ JavaScript/Js-Ts/aulas/aula15/ex/script.js
@@ -3,9 +3,9 @@ let numeroEnviado = document.getElementById('numero-titulo')
 let texto = document.getElementById('texto')
 
 numeroEnviado.innerHTML = numeroPedido
-texto.innerHTML = `Raiz Quadrada: ${numeroPedido ** (1/2)}.<br/>`
-texto.innerHTML += `${numeroPedido} é inteiro?: ${Number.isInteger(numeroPedido)}.<br/>`
-texto.innerHTML += `É NaN?: ${Number.isNaN(numeroPedido)}.<br/>`
-texto.innerHTML += `Arredondado para baixo: ${Math.floor(numeroPedido)}.<br/>`
-texto.innerHTML += `Arredondado para cima: ${Math.ceil(numeroPedido)}.<br/>`
-texto.innerHTML += `Com duas casas decimais: ${numeroPedido.toFixed(2)}<br/>`
+texto.innerHTML = `<p>Raiz Quadrada: ${numeroPedido ** (1/2)}</p>`
+texto.innerHTML += `<p>${numeroPedido} é inteiro?: ${Number.isInteger(numeroPedido)}</p>`
+texto.innerHTML += `<p>É NaN?: ${Number.isNaN(numeroPedido)}</p>`
+texto.innerHTML += `<p>Arredondado para baixo: ${Math.floor(numeroPedido)}</p>`
+texto.innerHTML += `<p>Arredondado para cima: ${Math.ceil(numeroPedido)}</p>`
+texto.innerHTML += `<p>Com duas casas decimais: ${numeroPedido.toFixed(2)}</p>`
