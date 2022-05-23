ecf94f2db57397bee3e88d3cf2a09cf4dd819f10
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-22 21:48:11 -0300

    Update arrays.js

--- JavaScript/Js-Ts/aulas/aula16/arrays.js
+++ JavaScript/Js-Ts/aulas/aula16/arrays.js
@@ -14,3 +14,21 @@ console.log(pontuacao[3]);
 
 pontuacao[3] = 7
 console.log(pontuacao[3]);
+
+console.log(pontuacao.length) //para saber o tamanho do array
+pontuacao[5] = 12 //permite adicionar um elemento manualmete no final da array
+console.log(pontuacao);
+alunos.push('Felipe') // adicionar automaticamente um elemento no final da array
+alunos.push('Marina')
+alunos.push('Josoé')
+console.log(alunos);
+
+alunos.unshift('Luiza') // adiciona um elemento no incio do array
+console.log(alunos);
+
+alunos.pop() //remove o ultimo elemento
+console.log(alunos);
+
+const removido = alunos.pop() //remove o ulltimo elemento e jogo numa nova constante
+console.log(alunos);
+console.log(removido);
