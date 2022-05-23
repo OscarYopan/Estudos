1d008b081491831764daee67017e7b99fb14b975
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-23 18:09:47 -0300

    Update objetosBasico.js

--- JavaScript/Js-Ts/aulas/aula18/objetosBasico.js
+++ JavaScript/Js-Ts/aulas/aula18/objetosBasico.js
@@ -20,15 +20,41 @@ console.log(pessoa1.nota);
 // FUNCAO PARA CRIAR OBJETOS
 
 function criaPessoa(nome, sobrenome, idade, nota) {
-  return{
-    nome: nome,
-    sobrenome: sobrenome,
-    idade: idade,
-    nota: nota
-  }
+  return{nome, sobrenome, idade, nota}
 }
 
 const pessoa2 = criaPessoa('Amanda', 'Yopan', 25, 9);
 const pessoa3 = criaPessoa('Juarex', 'Macedo', 87, 5);
-const pessoa3 = criaPessoa('Vitor', 'Hugo', 2, 1);
+const pessoa4 = criaPessoa('Vitor', 'Hugo', 2, 1);
+
+console.log(pessoa1.nome, pessoa2.nome, pessoa3.nome, pessoa4.nome);
+
+// FUNCAO/METODO dentro de OBJETOS
+
+const pessoa5 = {
+  nome: 'Guto',
+  sobrenome: 'Cell',
+  idade: 31,
+  nota: 8,
+  contagem: 1,
+
+  fala() {
+    console.log(`${this.nome} ${this.sobrenome} tem ${this.idade} está a procura de um novo amor!`);
+  },
+
+  contador(){
+    this.contagem++;
+  }
+
+};
+
+pessoa5.fala()
+pessoa5.contador()
+console.log(pessoa5.contagem);
+pessoa5.fala()
+pessoa5.contador()
+console.log(pessoa5.contagem);
+pessoa5.fala()
+pessoa5.contador()
+console.log(pessoa5.contagem);
 
