156a7af1b6c8098e8d37c2b9003a8e67ed8ad9d4
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-23 17:26:22 -0300

    Update funcoesBasico.js

--- JavaScript/Js-Ts/aulas/aula17/funcoesBasico.js
+++ JavaScript/Js-Ts/aulas/aula17/funcoesBasico.js
@@ -33,3 +33,30 @@ function soma(x, y) {
 }
 
 console.log(soma(1, 2));
+
+// Funcao com parametros pré definidos
+function somar( x= 1,y=2){
+  let total = x + y
+  return total
+}
+
+console.log(somar()); // se nao definir nenhum paramentro ele usa-ra a pré definição.
+console.log(somar(4)); //posso definir apenas um valor para x
+console.log(somar(4, 5)); // se defino valores para x e y ele nao usara a pre definição
+
+// FUNCAO ANONIMA
+
+let raiz = function(n){ //funcao sem nome pois a variavel já recebeu um nome
+  return n ** (1/2)
+}; //nesse caso precia terminar com ';'
+
+console.log(raiz(9));
+console.log(raiz(4));
+
+// ARROW FUNCTION
+
+let raizQuadrada = n => n ** (1/2); //quando a função só teria uma linha dentros das chaves {}
+
+console.log(raizQuadrada(9));
+console.log(raizQuadrada(4));
+console.log(raizQuadrada(25));
