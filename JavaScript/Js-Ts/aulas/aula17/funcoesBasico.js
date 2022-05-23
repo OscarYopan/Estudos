5309c118a2e41278a56bce1ee36ad9975ad3487d
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-22 22:41:06 -0300

    Update funcoesBasico.js

--- JavaScript/Js-Ts/aulas/aula17/funcoesBasico.js
+++ JavaScript/Js-Ts/aulas/aula17/funcoesBasico.js
@@ -1,6 +1,9 @@
 //FUNCÔES
 /*
 - Programas são basicamente um consjunto de funções que funcionam de maneira organizada e seguindo padrões pré estabelecidos.
+- O que está dentro de uma função só pode ser acessado pela própria função não por nada fora da funçõa.
+- Fora da função temos acesso ao o que a função retorna e elementos de escopo global.
+- Funções não execultam nada abaixo do 'return'
 */
 
 //criando uma função
@@ -25,7 +28,8 @@ console.log(variavel);
 //ex funcao
 function soma(x, y) {
   let total = x + y
-  return total  
+  return total
+  console.log('Ola Mundo'); // não é executado, pois está abaixo do array.
 }
 
 console.log(soma(1, 2));
