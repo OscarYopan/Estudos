21b68291f8ecf11b414380cccfeebb119583270e
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-27 22:22:45 -0300

    Create operadoresComparacao.js

---
+++ JavaScript/Js-Ts/aulas/aula21/operadoresComparacao.js
@@ -0,0 +1,17 @@
+//OPERADORES DE COMPARAÇÂO
+/*
+  - Sempre retornam um valor bollean (true ou false)
+  -     
+*/
+/*
+> - Maior que
+< - Menor que
+>= - Maior ou igual a
+<= - Menor ou igual a
+== - igualdade (valor)
+=== - igualdade estrita (valor e tipo)
+!= - diferente
+!== - diferente estrito (valor e tipo)
+*/
+
+console.log(10 > 5);
