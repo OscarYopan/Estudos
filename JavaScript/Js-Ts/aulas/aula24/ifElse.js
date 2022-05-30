a9135e026051b3ef0c26a02bbf5007eedd52586c
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-30 14:01:53 -0300

    Update ifElse.js

--- JavaScript/Js-Ts/aulas/aula24/ifElse.js
+++ JavaScript/Js-Ts/aulas/aula24/ifElse.js
@@ -4,6 +4,7 @@
 - 'else' só pode ser usado depois que já foi usado um 'if', pode ser usado uma única vez
 - 'else if' posso usar quantas vezes forem necessarias
 - pode-se usar somente o 'if' e 'else'
+= o código só é executado até achar um consdição verdadeira, o que vier abaixo não será executado.
 */
 
 const hora = 10
@@ -24,4 +25,16 @@ if(hora2 > 0 && hora2 < 12){
   console.log('Boa noite!');
 }else{
   console.log('Hora inválida!');
+}
+
+const numero = 10
+
+if(numero >= 0 && numero <= 5){
+  console.log('O número está entre 0 e 5');
+}else if (numero > 5 && numero < 9){
+  console.log('O número  está entre 6 e 8');
+}else if (numero >= 9 && numero <= 10){
+  console.log('O número  está entre 9 e 10');
+}else{
+  console.log('O numero não atende aos parametros');
 }
