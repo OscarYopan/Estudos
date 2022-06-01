5040aa3e6e13d4f4a3146cb1b065960870a70c1c
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-05-31 23:23:01 -0300

    -

--- JavaScript/Js-Ts/aulas1-26/aula26/assets/js/main.js
+++ JavaScript/Js-Ts/aulas1-26/aula26/assets/js/main.js
@@ -4,18 +4,26 @@ const form = document.querySelector('.form')
 //parar de recarregar a página
 form.addEventListener('submit', function(event){
   event.preventDefault();
-  console.log('Prevenido!');
+  calculo()
 })
 
-function calculo(){
+function calculo(msg){
   const peso = document.querySelector('#peso');
   const altura = document.querySelector('#altura');
   const res = document.querySelector('.resultado');
-  let imc = peso / altura
+  let imc = peso / (altura/100)
 
-  // if(imc <= 18.5){
-  //   res.innerHTML = (`Seu IMC é: ${imc}`);
-  // }
-
-  res.innerHTML = (`Seu IMC é: tanto`);
+  if(imc <= 18.5){
+    res.innerHTML = (`Seu IMC é: ${imc}`);
+  }else if(imc > 18.5 && imc < 25){
+    res.innerHTML = (`Seu IMC é: ${imc}`);
+  }else if(imc >= 25 && imc < 30){
+    res.innerHTML = (`Seu IMC é: ${imc}`);
+  }else if(imc >= 30 && imc < 35){
+    res.innerHTML = (`Seu IMC é: ${imc}`);
+  }else if(imc >= 35 && imc < 40){
+    res.innerHTML = (`Seu IMC é: ${imc}`);
+  }else if (imc >= 40){
+    res.innerHTML = (`Seu IMC é: ${imc}`);
+  }
 }
