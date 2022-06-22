15077a8ce80f4d73283f53d9485a9c1bf1d20271
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-06-22 20:34:29 -0300

    porject-sign-up=form start

--- JavaScript/wazana-js-iniciantes/Objetos/objetos.js
+++
@@ -1,9 +0,0 @@
-let pessoa = { // objetos são colocados dentro de chaves
-  nomeCompleto: "Oscar Yopan", // chave: valor
-  idade: 35, //Para mais de uma chave usar ','
-  peso: 147,    
-}
-pessoa.idade = 33 //para alterar um atributo de uma variavel ou objeto
-pessoa['nomeCompleto'] = 'Oscar Luis Yopan' //Outra maneira de modificar atributo
-console.log(pessoa) // mostra todo o objeto
-console.log(pessoa.idade) //mostra apenas uma variavel de dentro do objeto
