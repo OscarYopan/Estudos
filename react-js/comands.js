106ee852be696959030cbb864730e8580a6f47bf
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-07-28 22:28:17 -0300

    ---

---
+++ react-js/comands.js
@@ -0,0 +1,23 @@
+//npx create-react-app . //cria o projeto na pasta atual
+
+//npx create-react-app project //cria uma nova pasta com o projeto dentro
+
+//npm start //Inicia a execução do seu projeto
+
+//npm run build //Prepara todo o projeto para ser upado de uma forma mais otimizada
+
+//----//
+
+//Componente Sem Estado 'Stateless'
+
+function NomeApp() {
+  return(
+    <h1>Corpo da pagina em Json</h1>
+  )
+}
+
+
+
+//-------- Abaixo Comanandos Ainda por Aprender -------//
+//"test": "react-scripts test",
+//"eject": "react-scripts eject"
--- react-js/projeto1/src/App.js
+++ react-js/projeto1/src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Estou começando a aprendear React JS
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/OscarYopan"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Oscar Yopan Github Perfil
-        </a>
-      </header>
-    </div>
-  );
-}
+
+// Componente de Função
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Estou começando a aprendear React JS
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://github.com/OscarYopan"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Oscar Yopan Github Perfil
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App; // exporta a função e não o arquivo inteiro
