2ae21c2c4bc908cbc7e0818ad408e1db52ca027b
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-08-22 19:27:48 -0300

    ---

--- react-js/projeto1/src/App4.js
+++ react-js/projeto1/src/App4.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App4.css';
 import { Component } from 'react';
 
 class App extends Component {
@@ -28,28 +28,40 @@ class App extends Component {
 
   loadPosts = async () => {
     const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts')
+    // Tradução - Fetch -> Buscar
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos')
 
-    const [posts] = await Promise.all([postsResponse])
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse])
 
     const postsJson = await posts.json()
+    const photosJson = await photos.json()
 
-    this.setState({posts: postsJson})
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url }
+    })
+
+    this.setState({posts: postsAndPhotos})
   }
 
   render() {
     const { posts } = this.state
 
     return (
-      <div className='App'>   
-        {posts.map(post => (          
-          <div key={post.id}>
-            <h1 >{post.title}</h1>
-            <p>{post.body}</p>
-            <p>{post.body}</p>
-            <p>{post.body}</p>
-          </div>
-        ))}
-      </div>
+      <section className='container'>
+        <div className='posts'>   
+          {posts.map(post => (
+            <div className='post'>
+              <img src={post.cover} alt={post.title}/>
+              <div key={post.id} className='post-content'>
+                <h1 >{post.title}</h1>
+                <p>{post.body}</p>
+               <p>{post.body}</p>
+                <p>{post.body}</p>
+              </div>
+            </div>      
+          ))}
+        </div>
+      </section>
     );
   }
 }
