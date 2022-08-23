3875c755bafe21bd442f1b53c2c17e39f853717b
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-08-22 23:08:03 -0300

    --

--- react-js/projeto1/src/App4.js
+++ react-js/projeto1/src/App4.js
@@ -1,52 +1,28 @@
-import './App4.css';
-import { Component } from 'react';
-import { PostCard } from './components/Postcard';
+import { Component } from 'react'
+import './App4.css'
+import { Posts } from './components/Posts/index'
+import { loadPosts } from './utils/load-posts'
 
 class App extends Component {
-
   state = {
     posts: []
-  };
+  }
 
-  //Buscando Dados Externos
-
-  /*
-  API's Links:
-
-  https://jsonplaceholder.typicode.com/
-
-  https://jsonplaceholder.typicode.com/posts
-
-  https://jsonplaceholder.typicode.com/photos
-
-  https://developer.mozilla.org/pt-BR/docs/Web/API/Fetch_API
-
-  */
-
-  componentDidMount () {
-    this.loadPosts()
+  async componentDidMount() {
+    await this.loadPosts();
   }
 
   loadPosts = async () => {
-    this.setState({posts: postsAndPhotos})
+    const postsAndPhotos = await loadPosts();
+    this.setState({ posts: postsAndPhotos });
   }
 
   render() {
-    const { posts } = this.state
+    const { posts } = this.state;
 
     return (
-      <section className='container'>
-        <div className='posts'>   
-          {posts.map(post => (
-            <PostCard
-            key={post.id}
-            title={post.title}
-            body={post.body}
-            id={post.id}
-            cover={post.cover}
-            />      
-          ))}
-        </div>
+      <section className="container">
+        <Posts posts={posts} />
       </section>
     );
   }
