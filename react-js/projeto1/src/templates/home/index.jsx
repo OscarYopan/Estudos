8bb5c9416ae38c138443858f050d7fdb8b1e69cb
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-08-23 15:36:39 -0300

    Update index.jsx

--- react-js/projeto1/src/templates/home/index.jsx
+++ react-js/projeto1/src/templates/home/index.jsx
@@ -1,30 +1,31 @@
-import { Component } from 'react'
+import { Component } from 'react';
 
-import './styles.css'
+import './styles.css';
 
-import { Posts } from '../../components/Posts/index'
+import { Posts } from '../../components/Posts';
 import { loadPosts } from '../../utils/load-posts'
-import { Button } from '../../components/Button'
+import { Button } from '../../components/Button';
 
 export class Home extends Component {
   state = {
     posts: [],
     allPosts: [],
     page: 0,
-    postPerPage: 10
-  }
+    postsPerPage: 10
+  };
 
   async componentDidMount() {
     await this.loadPosts();
   }
 
   loadPosts = async () => {
-    const { page, postPerPage } = this.state
+    const { page, postsPerPage } = this.state;
+
     const postsAndPhotos = await loadPosts();
-    this.setState({ 
-      posts: postsAndPhotos.slice(page, postPerPage), //'slice()' permite pegar uma fatia do array, sendo definido o começõ e o fim da fatia.
-      AllPosts: postsAndPhotos,
-     });
+    this.setState({
+      posts: postsAndPhotos.slice(page, postsPerPage),
+      allPosts: postsAndPhotos,
+    });
   }
 
   loadMorePosts = () => {
@@ -39,22 +40,21 @@ export class Home extends Component {
     posts.push(...nextPosts);
 
     this.setState({ posts, page: nextPage });
-    console.log(123);
   }
 
   render() {
     const { posts, page, postsPerPage, allPosts } = this.state;
-    const noMorePosts = page + postsPerPage >= allPosts.length
+    const noMorePosts = page + postsPerPage >= allPosts.length;
 
     return (
       <section className="container">
         <Posts posts={posts} />
-        
-        <div className='button-container'>
-          <Button 
-          text='Load More Posts' 
-          onClick={this.loadMorePosts}
-          disabled={noMorePosts}
+
+        <div class="button-container">
+          <Button
+            text="Load more posts"
+            onClick={this.loadMorePosts}
+            disabled={noMorePosts}
           />
         </div>
       </section>
