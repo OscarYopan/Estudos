01d7b8dacaf262440eb9d3eb6c588f88e7b78e3a
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-08-23 18:59:45 -0300

    --

--- react-js/projeto1/src/templates/home/index.jsx
+++ react-js/projeto1/src/templates/home/index.jsx
@@ -5,13 +5,14 @@ import './styles.css';
 import { Posts } from '../../components/Posts';
 import { loadPosts } from '../../utils/load-posts'
 import { Button } from '../../components/Button';
+import { TextInput } from '../../components/TextInput';
 
 export class Home extends Component {
   state = {
     posts: [],
     allPosts: [],
     page: 0,
-    postsPerPage: 3,
+    postsPerPage: 10,
     searchValue: ''
   };
 
@@ -53,17 +54,30 @@ export class Home extends Component {
     const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
     const noMorePosts = page + postsPerPage >= allPosts.length;
 
+    const filteredPosts = !!searchValue ? 
+    allPosts.filter(post => {
+      return post.title.toLowerCase().includes(searchValue.toLowerCase());
+    })
+    : posts
+
     return (
       <section className="container">
-        {!!searchValue && (<h1>Search Value: {searchValue}</h1>)}
-        
-        <input
-        onChange={this.handleChange}
-        value={searchValue}
-        type="serch"
-        /> <br /> <br />
+        <div className="search-container">
+          
+          {/* {!!searchValue && (
+          <h1>Search Value: {searchValue}</h1>
+          )} */}
+          
+          <TextInput searchValue={searchValue} handleChange={this.handleChange}/>
+        </div>
 
-        <Posts posts={posts} />
+        {filteredPosts.length > 0 && (
+          <Posts posts={filteredPosts} />
+        )}      
+        
+        {filteredPosts.length === 0 && (
+          <p>Não encontramos post com essa palavra!</p>
+        )} 
 
         <div className="button-container">
           {!searchValue && (
