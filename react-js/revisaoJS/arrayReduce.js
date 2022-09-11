cbc7d9c848bc5fdce51669fed651fedd75427263
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-09-11 10:59:21 -0300

    Create arrayReduce.js

---
+++ react-js/revisaoJS/arrayReduce.js
@@ -0,0 +1,31 @@
+//Reduce:
+/*
+  - 
+*/
+
+//      Indice   0  1   2   3  4  5  6  7  8  9   10  11  12
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 18, 22, 27]
+
+
+
+
+
+
+
+
+
+
+//---//---//---//---//
+
+
+const pessoas = [
+  { nome: 'Oscar', idade: 35 },
+  { nome: 'Amanda', idade: 25},
+  { nome: 'Guto', idade: 32},
+  { nome: 'Felipe', idade: 30},
+  { nome: 'Marina', idade: 28},
+  { nome: 'Cris', idade: 30}
+]
+
+//Para cada elemento:
+//retorne uma string com o nome da pessoa
