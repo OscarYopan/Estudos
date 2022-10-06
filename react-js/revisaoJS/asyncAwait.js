00dc12be43a6191e69db87f1e222a4d5599294ce
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-10-06 19:14:35 -0300

    Update asyncAwait.js

--- react-js/revisaoJS/asyncAwait.js
+++ react-js/revisaoJS/asyncAwait.js
@@ -1,4 +1,6 @@
-function numeroAleatorio(min, max) {
+// ASYNG AWAIT
+
+function numeroAleatorio(min = 0, max = 3) {
   min *= 1000;
   max *= 1000;
   return Math.floor(Math.random() * (max - min) + min)
@@ -7,7 +9,7 @@ function numeroAleatorio(min, max) {
 function esperaAi(msg, tempo) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (typeof msg !== 'string ') {
+      if (typeof msg !== 'string') {
         reject('Cai no Erro')
         return
       }
@@ -18,5 +20,37 @@ function esperaAi(msg, tempo) {
   });
 }
 
+/*
 esperaAi('Fase 1', numeroAleatorio(0, 3))
-  .then(valor => console.log(valor);)
+  .then(valor => {
+    console.log(valor);
+    return esperaAi('Fase2', numeroAleatorio())
+  })
+  .then(fase => {
+    console.log(fase);
+    return esperaAi('Fase 3')
+  })
+  .then(fase => {
+    console.log(fase);
+  })
+  .catch(e => console.log(e)
+*/
+
+async function executa() {
+
+  try {
+    const fase1 = await esperaAi('Fase 1', numeroAleatorio());
+    console.log(fase1);
+  
+    const fase2 = await esperaAi('Fase 2', numeroAleatorio());
+    console.log(fase2);
+  
+    const fase3 = await esperaAi('Fase 3', numeroAleatorio());
+    console.log('Terminamos na Fase: ', fase3);
+  } catch(e) {
+    console.log('Errou Feio!');
+  }
+
+}
+
+executa()
