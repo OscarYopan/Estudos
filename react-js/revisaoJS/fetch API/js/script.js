8bfa08a7b87062dea580512ca9d617625ae0d921
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-11-04 18:39:48 -0300

    Update script.js

--- react-js/revisaoJS/fetch API/js/script.js
+++ react-js/revisaoJS/fetch API/js/script.js
@@ -5,30 +5,32 @@ const postsContainer = document.querySelector('#posts-container')
 
 //Get All Posts
 async function getAllPosts() {
-  const response = await fetch(url)
+  const response = await fetch(url);
 
-  //Abaixo me permite recebere um array de objetos
-  const data = await response.json()
+  console.log(response);
 
-  loadingElement.classList.add('hide')
+  const data = await response.json();
+
+  console.log(data);
+
+  loadingElement.classList.add("hide");
 
   data.map((post) => {
-    const div = document.createElement('div')
-    const title = document.createElement('h2')
-    const body = document.createElement('p')
-    const link = document.createElement('a')
-  
+    const div = document.createElement("div");
+    const title = document.createElement("h2");
+    const body = document.createElement("p");
+    const link = document.createElement("a");
+
     title.innerText = post.title;
     body.innerText = post.body;
-    link.innerText = 'Ler'
-    link.setAttribute('href', `/post.html?id=${post.id}`)
-  
-    div.appendChild(title)
-    div.appendChild(body)
-    div.appendChild(link)
-  
-    postsContainer(div)
-  })
+    link.innerText = "Ler";
+    link.setAttribute("href", `/post.html?id=${post.id}`);
+
+    div.appendChild(title);
+    div.appendChild(body);
+    div.appendChild(link);
+    postsContainer.appendChild(div);
+  });
 }
 
 getAllPosts()
