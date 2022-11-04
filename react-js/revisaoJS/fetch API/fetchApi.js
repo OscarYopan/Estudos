5e4e30e94aad2a8a15532671ba9a77d1c63e2a68
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-11-04 17:55:53 -0300

    --

--- react-js/revisaoJS/fetch API/fetchApi.js
+++ react-js/revisaoJS/fetch API/fetchApi.js
@@ -1,4 +1,8 @@
-fetch('index.html')
-.then(resposta => {
-  if (resposta.status !== 200) 
-})
+// Fetch API
+/*
+- Pode ser utilizada com 'Promise' ou 'assyng/await';
+- Se a requisição for com 'GET' é preciso apenas passar a URL do recurso que estamos requisitando da API;
+- Caso seja outro verbo, podemos configurar a requisição com: 'method', 'body' e 'headers';
+- Muito usados com frameworks/libs.
+Fonte: https://www.youtube.com/watch?v=qIGYM4S8x50
+*/
