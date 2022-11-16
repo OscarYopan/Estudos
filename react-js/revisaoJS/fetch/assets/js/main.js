5fe9c68871eeefc992d262f06ab6ac0e8291223e
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-11-16 17:06:11 -0300

    --

--- react-js/revisaoJS/fetch/assets/js/main.js
+++ react-js/revisaoJS/fetch/assets/js/main.js
@@ -1,5 +1,21 @@
-fetch('https://dummyjson.com/products')
+fetch('https://dummyjson.com/products/5')
 .then(res => res.json())
+.then(anounce => {
+  createanouce(anounce)
+})
 .then(console.log);
+
+function createanouce(anounce) {
+  const resultado = document.querySelector('.resultado')
+
+  resultado.innerHTML = `
+  <div><img src="${anounce.thumbnail}" alt=""></div>
+  <h3>${anounce.title}</h3>
+  <p>${anounce.description}</p>
+  <p>$ ${anounce.price},99 a vista</p>
+  `
+}
+
+
 
 
