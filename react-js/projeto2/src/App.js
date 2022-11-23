e010f3e7fc4242c9d435266b7c83f934b12c2ec5
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-11-23 15:37:39 -0300

    Update App.js

--- react-js/projeto2/src/App.js
+++ react-js/projeto2/src/App.js
@@ -1,10 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-//import { Component } from 'react';
 
 function App() {
-  //const { reverse } = this.state;
   const [reverse, setReverse] = useState(false);
   const reverseClass = reverse ? 'reverse' : '';
 
@@ -23,40 +21,5 @@ function App() {
     </div>
   );
 }
-
-/*
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleClick.bind(this);
-
-    this.state = {
-      reverse: false,
-    };
-  }
-
-  handleClick = () => {
-    const { reverse } = this.state;
-    this.setState({ reverse: !reverse });
-  };
-
-  render() {
-    const { reverse } = this.state;
-    const reverseClass = reverse ? 'reverse' : '';
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-          <button onClick={this.handleClick} type="button">
-            Reverter
-          </button>
-        </header>
-      </div>
-    );
-  }
-}
-
-*/
 
 export default App;
