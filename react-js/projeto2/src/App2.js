b674d20318bb7f372e212d31fd167c454dc72408
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-11-28 11:55:59 -0300

    Update App2.js

--- react-js/projeto2/src/App2.js
+++ react-js/projeto2/src/App2.js
@@ -1,13 +1,33 @@
 //Hooks - Useffect
 /*
   - https://www.udemy.com/course/curso-de-reactjs-nextjs-completo-do-basico-ao-avancado/learn/lecture/24955272#overview
+  - 'componentDidUpdate' = executa toda vez que o componente executa.
+    - recebe como primeiro componente uma função;
+  - 'componentDidMount' = execulta uma vez
+    - necessita passar um segundo parametro, um array com dependencias.
+    - caso passe um array vazio, executa somente uma vez
 */
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [counter, setCounter] = useState(0);
+
+  //componentDidUpdate
+  useEffect(() => {
+    console.log('componentDidUpdate');
+  });
+
+  //componentDidMount
+  useEffect(() => {
+    console.log('componentDidMount');
+  }, []);
+
+  //componentDidMount
+  useEffect(() => {
+    console.log('componentDidMount');
+  }, []);
 
   return (
     <div className="App">
