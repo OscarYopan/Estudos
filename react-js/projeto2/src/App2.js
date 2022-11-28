76a32ae642a5af68741d79361fe0c6f999678f9e
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-11-28 11:43:43 -0300

    --

--- react-js/projeto2/src/App2.js
+++ react-js/projeto2/src/App2.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+//Hooks - Useffect
+/*
+  - https://www.udemy.com/course/curso-de-reactjs-nextjs-completo-do-basico-ao-avancado/learn/lecture/24955272#overview
+*/
+
 import { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <h1>Oi</h1>
+      <h1>Contador: {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
     </div>
   );
 }
