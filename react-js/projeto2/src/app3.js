3399e2880c2e184799e87e4f92933337075a5a14
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-11-29 15:15:46 -0300

    Update app3.js

--- react-js/projeto2/src/app3.js
+++ react-js/projeto2/src/app3.js
@@ -2,16 +2,29 @@
 /*
   -
 */
+import P from 'prop-types';
 import { useState } from 'react';
 import './App.css';
+
+button.prototypes = {
+  incrementButton: P.func,
+};
+
+const button = (incrementButton) => {
+  return <button onClick={() => incrementButton(10)}>+</button>;
+};
 
 function App() {
   const [counter, setCounter] = useState(0);
 
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  };
+
   return (
     <div className="App">
       <h1>Contador: {counter}</h1>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <button onClick={incrementCounter}>+</button>
     </div>
   );
 }
