baf7bc601533573be3f4293c3a2c1798f9ba2e2e
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-11-29 15:04:42 -0300

    Update app3.js

--- react-js/projeto2/src/app3.js
+++ react-js/projeto2/src/app3.js
@@ -1,41 +1,12 @@
-//Hooks - Useffect
+//Hooks - UseCallBack
 /*
-  - https://www.udemy.com/course/curso-de-reactjs-nextjs-completo-do-basico-ao-avancado/learn/lecture/24955272#overview
-  - 'componentDidUpdate' = executa toda vez que o componente executa.
-    - recebe como primeiro componente uma função;
-  - 'componentDidMount' = execulta uma vez
-    - necessita passar um segundo parametro, um array com dependencias.
-    - caso passe um array vazio, executa somente uma vez
-    - cuidar para limpar as execuções automaticas para evitar erros em remontagens
-  - com dependencia = executa toda vez que a dependencia muda.
+  -
 */
-
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   const [counter, setCounter] = useState(0);
-
-  //componentDidUpdate
-  useEffect(() => {
-    console.log('componentDidUpdate');
-  });
-
-  //componentDidMount
-  useEffect(() => {
-    console.log('componentDidMount');
-    document.querySelector('h1')?.addEventListener('click', eventFn);
-
-    //componentWillEmout = limpeza
-    return () => {
-      document.querySelector('h1')?.removeEventListener('click', eventFn);
-    };
-  }, []);
-
-  //com dependencia
-  useEffect(() => {
-    console.log('contador mudou para:', counter);
-  }, [counter]);
 
   return (
     <div className="App">
@@ -44,9 +15,5 @@ function App() {
     </div>
   );
 }
-
-const eventFn = () => {
-  console.log('H1 clicado');
-};
 
 export default App;
