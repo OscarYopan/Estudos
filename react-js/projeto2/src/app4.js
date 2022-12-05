088104e9aedbac8c0f0d03911d49acf74e70f8c1
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-12-05 13:41:10 -0300

    --

--- react-js/projeto2/src/app4.js
+++ react-js/projeto2/src/app4.js
@@ -1,8 +1,40 @@
 //Hooks - UseMemo>
 /*
-  - Usar para coisas que são pesadas;
-  - Evita que funções que forem se repedir inteiramente seja executada novamente;
-  - 'React.memo(componente)' use rapa especifical o que deve ser callback;
-  *https://pt-br.reactjs.org/docs/hooks-reference.html#usecallback
-  *https://www.udemy.com/course/curso-de-reactjs-nextjs-completo-do-basico-ao-avancado/learn/lecture/24957558#overview
+  - Mesma função de 'UseCallBack';
+  - Nesse caso ele salva valores;
+  *https://www.udemy.com/course/curso-de-reactjs-nextjs-completo-do-basico-ao-avancado/learn/lecture/24960210?start=15#overview
 */
+
+import P from 'prop-types';
+import { useCallback, useState } from 'react';
+import './App.css';
+import React from 'react';
+
+const Button = React.memo(function Button({ incrementButton }) {
+  console.log('Filho');
+  return <button onClick={() => incrementButton(1)}>+</button>;
+});
+
+Button.propTypes = {
+  incrementButton: P.func,
+};
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  //UseCallBack - Formato:
+  const incrementCounter = useCallback((num) => {
+    setCounter((c) => c + num);
+  }, []);
+
+  console.log('Pai');
+
+  return (
+    <div className="App">
+      <h1>Contador: {counter}</h1>
+      <Button incrementButton={incrementCounter} />
+    </div>
+  );
+}
+
+export default App;
