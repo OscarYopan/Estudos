e29456b4b9a84ac0e7105e89b4712b692f8609d4
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-12-05 13:36:47 -0300

    Create app4.js

---
+++ react-js/projeto2/src/app4.js
@@ -0,0 +1,8 @@
+//Hooks - UseMemo>
+/*
+  - Usar para coisas que são pesadas;
+  - Evita que funções que forem se repedir inteiramente seja executada novamente;
+  - 'React.memo(componente)' use rapa especifical o que deve ser callback;
+  *https://pt-br.reactjs.org/docs/hooks-reference.html#usecallback
+  *https://www.udemy.com/course/curso-de-reactjs-nextjs-completo-do-basico-ao-avancado/learn/lecture/24957558#overview
+*/
