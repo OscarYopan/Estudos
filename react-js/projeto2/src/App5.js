18443beb4502de4dc96c5d8477f5b53dc3cd3b1f
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-12-06 15:22:54 -0300

    Update App5.js

--- react-js/projeto2/src/App5.js
+++ react-js/projeto2/src/App5.js
@@ -1,11 +1,12 @@
 //Hooks - UseRef
 /*
-  -
+  - Cria uma referencia para alguma coisa,
+  - Muito usado para pegar e manipular elemento da DOM,
   * https://reactjs.org/docs/hooks-reference.html#useref
   * https://www.udemy.com/course/curso-de-reactjs-nextjs-completo-do-basico-ao-avancado/learn/lecture/24969452?start=15#content
 */
 import P from 'prop-types';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 
 const Post = ({ post, handleClick }) => {
@@ -24,12 +25,14 @@ Post.propTypes = {
     title: P.string,
     body: P.string,
   }),
-  onclick: P.func,
+  handleClick: P.func,
 };
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [value, setValue] = useState('');
+  const input = useRef(null);
+  const contador = useRef(0);
 
   console.log('Pai foi Renderizado!');
 
@@ -40,14 +43,25 @@ function App() {
       .then((r) => setPosts(r));
   }, []);
 
+  useEffect(() => {
+    input.current.focus();
+    console.log(input.current);
+  }, [value]);
+
+  useEffect(() => {
+    contador.current++;
+  }, []);
+
   const handleClick = (value) => {
     setValue(value);
   };
 
   return (
     <div className="App">
+      <h5>Rendezou: {contador.current} vezes.</h5>
       <p>
         <input
+          ref={input}
           type="search"
           value={value}
           onChange={(e) => setValue(e.target.value)}
@@ -57,7 +71,7 @@ function App() {
         return (
           posts.length > 0 &&
           posts.map((post) => {
-            return <Post post={post} key={post.id} handleClick={handleclick} />;
+            return <Post post={post} key={post.id} handleClick={handleClick} />;
           })
         );
       }, [posts])}
