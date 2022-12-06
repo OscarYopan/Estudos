9175810d7876224635214e0e9bd0352441b82b7d
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-12-06 14:28:07 -0300

    Update App5.js

--- react-js/projeto2/src/App5.js
+++ react-js/projeto2/src/App5.js
@@ -1,10 +1,8 @@
-//Hooks - UseMemo
+//Hooks - UseRef
 /*
-  - Mesma função de 'UseCallBack';
-  - Nesse caso ele salva valores;
-  - Recebe uma função;
-  - Recebe um array com dependencias;
-  *https://www.udemy.com/course/curso-de-reactjs-nextjs-completo-do-basico-ao-avancado/learn/lecture/24960210?start=15#overview
+  -
+  *https://reactjs.org/docs/hooks-reference.html#useref
+  *https://www.udemy.com/course/curso-de-reactjs-nextjs-completo-do-basico-ao-avancado/learn/lecture/24969452?start=15#content
 */
 import P from 'prop-types';
 import { useEffect, useMemo, useState } from 'react';
@@ -67,39 +65,3 @@ function App() {
 }
 
 export default App;
-
-// import P from 'prop-types';
-// import { useCallback, useMemo, useState } from 'react';
-// import './App.css';
-// import React from 'react';
-
-// const Button = ({ incrementButton }) => {
-//   console.log('Filho');
-//   return <button onClick={() => incrementButton(1)}>+</button>;
-// };
-
-// Button.propTypes = {
-//   incrementButton: P.func,
-// };
-
-// function App() {
-//   const [counter, setCounter] = useState(0);
-
-//   //UseCallBack - Formato:
-//   const incrementCounter = useCallback((num) => {
-//     setCounter((c) => c + num);
-//   }, []);
-
-//   console.log('Pai');
-
-//   const btn = useMemo(() => {
-//     return <Button incrementButton={incrementCounter} />;
-//   }, [incrementCounter]);
-
-//   return (
-//     <div className="App">
-//       <h1>Contador: {counter}</h1>
-//       {btn}
-//     </div>
-//   );
-// }
