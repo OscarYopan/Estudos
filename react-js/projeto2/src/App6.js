c4452826f43ed525417a2184b625e80e96122d51
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-12-16 16:10:00 -0300

    --

---
+++ react-js/projeto2/src/App6.js
@@ -0,0 +1,17 @@
+//Hooks - UseContext
+/*
+  -
+  * https://www.udemy.com/course/curso-de-reactjs-nextjs-completo-do-basico-ao-avancado/learn/lecture/24971438?start=15#content
+  * https://pt-br.reactjs.org/docs/hooks-reference.html#usecontext
+*/
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Oi</h1>
+    </div>
+  );
+}
+
+export default App;
