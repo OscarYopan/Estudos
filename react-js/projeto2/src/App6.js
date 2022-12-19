cef9e22774efafbf07421393322c0133840485bf
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-12-19 14:58:16 -0300

    Update App6.js

--- react-js/projeto2/src/App6.js
+++ react-js/projeto2/src/App6.js
@@ -1,10 +1,26 @@
 //Hooks - UseContext
 /*
-  -
+  - Serve para repassar propriedades para um componente abaixo da arvore de componentes e subcomponentes;
+  - Para isso usam-se estadps Globais;
   * https://www.udemy.com/course/curso-de-reactjs-nextjs-completo-do-basico-ao-avancado/learn/lecture/24971438?start=15#content
   * https://pt-br.reactjs.org/docs/hooks-reference.html#usecontext
 */
+import React from 'react';
 import './App.css';
+
+// eslint-disable-next-line
+const globalState = {
+  title: 'Titulo do Contexto',
+  counter: 0,
+};
+
+// eslint-disable-next-line
+const globalContext = React.createContext();
+
+// eslint-disable-next-line
+const Div = ({ children }) => {
+  return <div className="App">{children}</div>;
+};
 
 function App() {
   return (
