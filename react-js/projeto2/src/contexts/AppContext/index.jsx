fcdc3f50e84743bf1bb262d9cf43d902dd4e020b
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-12-29 15:37:26 -0300

    --

--- react-js/projeto2/src/contexts/AppContext/index.jsx
+++ react-js/projeto2/src/contexts/AppContext/index.jsx
@@ -6,10 +6,10 @@ export const GlobalContext = createContext();
 
 // eslint-disable-next-line
 export const AppContext = ({ children }) => {
-  const [state, setState] = useState(globalState);
+  const [contextState, setState] = useState(globalState);
 
   return (
-    <GlobalContext.Provider value={(state, setState)}>
+    <GlobalContext.Provider value={(contextState, setState)}>
       {children}
     </GlobalContext.Provider>
   );
