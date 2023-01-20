34a70ab4f95400b04d73b0c30a6453f68cba84df
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-01-20 13:23:22 -0300

    --

--- react-js/projeto2/src/App8.js
+++ react-js/projeto2/src/App8.js
@@ -1,15 +1,27 @@
+import { useReducer } from 'react';
 import './App.css';
 
 export const globalState = {
-  title: 'O título que contexto',
-  body: 'O body do contexto',
+  title: 'O Título',
+  body: 'O body do contexto, que com certeza deve conter um texto consideravelmente maior que o próprio titulo, se não não faz sentido, até porque a cabeça sempre é menor ue o corpo. O body do contexto, que com certeza deve conter um texto consideravelmente maior que o próprio titulo, se não não faz sentido, até porque a cabeça sempre é menor ue o corpo.O body do contexto, que com certeza deve conter um texto consideravelmente maior que o próprio titulo, se não não faz sentido, até porque a cabeça sempre é menor ue o corpo.',
   counter: 0,
 };
 
+const reducer = (state) => {
+  return {
+    ...state,
+  };
+};
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <h1>{state.title}</h1>
+      <p>{state.body}</p>
+      <h2>{state.counter}</h2>
+      <button onClick={dispatch}>Click</button>
     </div>
   );
 }
