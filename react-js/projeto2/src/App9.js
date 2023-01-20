10611444dd3a38ea70fbc7562c6eb2bf5fb27229
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-01-20 14:45:06 -0300

    Update App9.js

--- react-js/projeto2/src/App9.js
+++ react-js/projeto2/src/App9.js
@@ -1,38 +1,74 @@
-import { Children } from 'react';
-import { createContext } from 'react';
-import './App.css';
+// ***************** useContex + useReduct ***********************
+
 import P from 'prop-types';
-import { useReducer } from 'react';
+import { createContext, useContext, useReducer, useRef } from 'react';
+import './App.css';
+
+// actions.js
+export const actions = {
+  CHANGE_TITLE: 'CHANGE_TITLE',
+};
 
 // data.js
 export const globalState = {
-  title: 'O Título',
-  body: 'O body do contexto, que com certeza deve conter um texto consideravelmente maior que o próprio titulo, se não não faz sentido, até porque a cabeça sempre é menor ue o corpo. O body do contexto, que com certeza deve conter um texto consideravelmente maior que o próprio titulo, se não não faz sentido, até porque a cabeça sempre é menor ue o corpo.O body do contexto, que com certeza deve conter um texto consideravelmente maior que o próprio titulo, se não não faz sentido, até porque a cabeça sempre é menor ue o corpo.',
+  title: 'O título que contexto',
+  body: 'O body do contexto',
   counter: 0,
 };
 
-//AppContext.jsx
-export const context = createContext();
+// reducer.js
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case actions.CHANGE_TITLE: {
+      console.log('Mudar título');
+      return { ...state, title: action.payload };
+    }
+  }
+
+  return { ...state };
+};
+
+// AppContext.jsx
+export const Context = createContext();
 export const AppContext = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, globalState);
 
-  return <context.Provider value={{ state }}>{Children}</context.Provider>;
+  const changeTitle = (payload) => {
+    dispatch({ type: actions.CHANGE_TITLE, payload });
+  };
+
+  return (
+    <Context.Provider value={{ state, changeTitle }}>
+      {children}
+    </Context.Provider>
+  );
 };
 
-AppContext.prototypes = {
+AppContext.propTypes = {
   children: P.node,
 };
 
-//reducer.js
-export const reducer = (state, action) => {
-  return { ...state };
+// H1/index.jsx
+export const H1 = () => {
+  const context = useContext(Context);
+  const inputRef = useRef();
+
+  return (
+    <>
+      <h1 onClick={() => context.changeTitle(inputRef.current.value)}>
+        {context.state.title}
+      </h1>
+      <input type="text" ref={inputRef} />
+    </>
+  );
 };
-//App.jsx
+
+// App.jsx
 function App() {
   return (
     <AppContext>
-      <div className="App">
-        <h1>Olá</h1>
+      <div>
+        <H1 />
       </div>
     </AppContext>
   );
