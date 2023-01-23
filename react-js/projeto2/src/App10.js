7b977c5bfabc24c6500e519d140c1e333b5d2521
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-01-23 13:42:20 -0300

    Update App10.js

--- react-js/projeto2/src/App10.js
+++ react-js/projeto2/src/App10.js
@@ -1,8 +1,27 @@
+// ********************** Criando meu Proprio Hook **************************
+
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+const useMyHook = (cb) => {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      cb();
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, [cb]);
+};
+
 // App.jsx
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  useMyHook(() => setCounter((c) => c + 1));
+
   return (
     <div>
-      <h1>Oi</h1>
+      <h1>Contador: {counter}</h1>
     </div>
   );
 }
