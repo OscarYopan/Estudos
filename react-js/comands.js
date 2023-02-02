3f5f52b58c64c0f24b7cac0319c6f42a6c877b72
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-02-02 09:03:06 -0300

    ---

--- react-js/comands.js
+++ react-js/comands.js
@@ -6,6 +6,8 @@
 
 //npm run build //Prepara todo o projeto para ser upado de uma forma mais otimizada
 
+//npm i react-router-dom //cria a router dom se quiser podem inluir @6 no final para escolher a versão 6, sem espaço.
+
 //-------- Abaixo Comanandos Ainda por Aprender -------//
 //"test": "react-scripts test",
 //"eject": "react-scripts eject"
