59b974a9efc780d82fe91b23922eb3d0e86021d8
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-02-02 09:27:08 -0300

    --

---
+++ react-js/projeto5/src/components/about/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+export const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores aut ut veniam, a eum laborum. Dicta iure hic id necessitatibus? Aliquid pariatur quos delectus sit vitae in provident error repellat? Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, iste unde! Fuga officiis ipsam non exercitationem, a magnam voluptatum nihil aliquam quis quos sit provident maiores accusamus autem dolores totam.</p>
+
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores aut ut veniam, a eum laborum. Dicta iure hic id necessitatibus? Aliquid pariatur quos delectus sit vitae in provident error repellat? Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, iste unde! Fuga officiis ipsam non exercitationem, a magnam voluptatum nihil aliquam quis quos sit provident maiores accusamus autem dolores totam.</p>
+    </div>
+  )
+}
--- react-js/projeto5/src/components/home/index.jsx
+++ react-js/projeto5/src/components/home/index.jsx
@@ -3,7 +3,7 @@ import './style.css'
 export const Home = () => {
   return (
   <div>
-    <h1>Oi</h1>
+    <h1>Home</h1>
   </div>
   )
 }
