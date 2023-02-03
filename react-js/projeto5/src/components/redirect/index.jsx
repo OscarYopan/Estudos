2b53fc22d7a738c6c43babda527d42c346d0082e
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-02-03 09:20:15 -0300

    Update index.jsx

--- react-js/projeto5/src/components/redirect/index.jsx
+++ react-js/projeto5/src/components/redirect/index.jsx
@@ -1,10 +1,29 @@
 import './style.css';
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export const Redirect = () => {
-  const [time, setTime] = useState()
+  const [time, setTime] = useState(3)
+  const timeout = useRef(0)
+  const navigate = useNavigate()
 
- return (
+  useEffect(() => {
+    clearTimeout(timeout.current)
+
+    timeout.current = setTimeout(() => {
+      setTime((t) => t - 1)
+    }, 1000)
+
+    if (time <= 0) {
+      navigate('/')
+    }
+
+    return () => {
+      clearTimeout(timeout.current)
+    }
+  }, [time])
+
+  return (
     <div>
       <h1>Redirect</h1>
     </div>
