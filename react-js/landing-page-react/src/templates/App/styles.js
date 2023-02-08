a585c3ae0b0688bb7da4cd617250263d779b995f
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-02-08 13:45:08 -0300

    --

--- react-js/landing-page-react/src/templates/App/styles.js
+++ react-js/landing-page-react/src/templates/App/styles.js
@@ -1,5 +1,16 @@
+import { css } from 'styled-components'
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
-  background: red;
+  ${() => css`
+    background: #949ba6;
+  `}
 `
+
+/*
+color1 : #949BA6
+color2 : #5ED7F2
+color3 : #F2E49B
+color4 : #F2D091
+color5 : #A69068
+*/
