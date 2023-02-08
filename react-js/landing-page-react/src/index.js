a66a037b5c941ccdad61b652d0feda9eaf437bdf
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-02-08 13:29:30 -0300

    --

--- react-js/landing-page-react/src/index.js
+++ react-js/landing-page-react/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './templates'
+import Home from './templates/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
--- react-js/landing-page-react/src/templates/index.jsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-    <div className="App">
-      <h1>Olá</h1>
-    </div>
-  );
-}
-
-export default Home;
