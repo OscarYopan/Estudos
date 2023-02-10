0f662f1398cac26e5b8ed601d131c0cfff505029
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-02-10 15:09:37 -0300

    --

--- react-js/landing-page-react/src/templates/App/App.test.jsx
+++ react-js/landing-page-react/src/templates/App/App.test.jsx
@@ -1,10 +1,19 @@
+/* eslint-disable testing-library/no-node-access */
 import { renderTheme } from '../../styles/reder-theme';
 import Home from './index';
 import { screen } from '@testing-library/react';
+import { theme } from '../../styles/themes';
 
 
 test('renders learn react link', () => {
   renderTheme(<Home />);
+  // eslint-disable-next-line no-unused-vars
+  const { debug } = renderTheme(<Home />);
   const headingContainer = screen.getByRole('heading', { name: 'Hello' })
     .parentElement;
+  expect(headingContainer).toHaveStyle({
+    background: theme.colors.mainBg,
+  });
+  expect(headingContainer).toMatchSnapshot();
+  expect(headingContainer).toHaveStyleRule('background', 'red');
 });
--- react-js/landing-page-react/src/templates/App/styles.js
+++ react-js/landing-page-react/src/templates/App/styles.js
@@ -6,11 +6,3 @@ export const Wrapper = styled.div`
     background: #949ba6;
   `} */
 `
-
-/*
-color1 : #949BA6
-color2 : #5ED7F2
-color3 : #F2E49B
-color4 : #F2D091
-color5 : #A69068
-*/
