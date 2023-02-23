412419dd8d6ed1e975698d861f135416e4cf9cce
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-02-23 16:56:50 -0300

    Update index.js

--- react-js/18-Projects/my-app/src/index.js
+++ react-js/18-Projects/my-app/src/index.js
@@ -1,22 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-function Hello() {
+// Nest Components
+function App() {
+  return (
+    <div className="App">
+      <Hello />
+      <Botao />
+    </div>
+  )
+}
+
+const Hello = () => {
   return (
     <div className="exemploName">
       <h1>Hello World!</h1>
       <ul>
         <li>First</li>
         <li>Second</li>
-        <li>Thirst</li>
-        <li>Forth</li>
       </ul>
       <input type="text" />
     </div>
   )
 }
 
-export default Hello
+const Botao = () => {
+  return (
+    <div className="Botao">
+      <h3>Novo Mundo!</h3>
+    </div>
+  )
+}
+
+export default App
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
