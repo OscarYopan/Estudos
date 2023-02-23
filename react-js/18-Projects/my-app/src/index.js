69ac17776d6c7fe28cb7ded2608dd187d42d07fe
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-02-23 16:30:15 -0300

    Update index.js

--- react-js/18-Projects/my-app/src/index.js
+++ react-js/18-Projects/my-app/src/index.js
@@ -4,47 +4,7 @@ import ReactDOM from 'react-dom/client'
 function Hello() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sequi
-        placeat nesciunt impedit exercitationem? Placeat itaque laboriosam, sed
-        accusantium consectetur ea nulla corrupti asperiores commodi repellendus
-        rem voluptatem ex mollitia! Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. Laudantium temporibus quia ratione ipsum in excepturi
-        aliquam voluptatibus est laborum quaerat culpa, distinctio id illum
-        veritatis nisi aut explicabo nobis! Eligendi?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe
-        reprehenderit molestiae delectus, nostrum sint minus ea, dolorem ipsam
-        dignissimos rerum iusto id. Similique aspernatur accusantium, tempore
-        repellendus ducimus maiores adipisci! Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Quas adipisci est ipsam velit dolore illo,
-        eveniet perferendis praesentium nisi, possimus nostrum dolores fugit
-        deleniti quaerat error ea nulla doloribus molestiae. Lorem ipsum dolor
-        sit amet consectetur adipisicing elit. Fugit possimus cumque labore eum
-        laborum veritatis, fuga placeat, quod voluptatem porro odit aspernatur
-        praesentium quae. Similique laborum fuga veniam commodi minima?
-      </p>
-      <h2>Hello Brazil</h2>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt, placeat
-        expedita culpa ipsam hic possimus modi consectetur labore a veniam? Vero
-        harum earum soluta, aliquam sunt voluptas laudantium non ipsum? Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Illum est architecto
-        nostrum exercitationem ratione, suscipit saepe labore quia qui neque
-        doloribus totam quae quasi, voluptates dolorem veritatis esse, voluptate
-        eius?
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi
-        reiciendis veniam fugit corrupti est in, sequi, mollitia commodi
-        praesentium porro quaerat voluptate dolorem laudantium repellendus. Quae
-        corrupti fugiat sint accusamus. Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. Repudiandae aliquid ratione vitae. Nisi, cumque, ullam
-        consequatur dolorum in omnis ab iste dolorem, ipsum exercitationem
-        quibusdam adipisci sunt qui officia fugit?
-      </p>
+      <h1>Hello World!</h1>
     </div>
   )
 }
