d17fe574f4dc647028045e52a243ed3831762ccd
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-02-23 16:39:06 -0300

    Update index.js

--- react-js/18-Projects/my-app/src/index.js
+++ react-js/18-Projects/my-app/src/index.js
@@ -5,6 +5,12 @@ function Hello() {
   return (
     <div>
       <h1>Hello World!</h1>
+      <ul>
+        <li>First</li>
+        <li>Second</li>
+        <li>Thirst</li>
+        <li>Forth</li>
+      </ul>
     </div>
   )
 }
