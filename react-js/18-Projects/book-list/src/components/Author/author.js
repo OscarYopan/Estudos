e46378f10faf6bdbfb9e8ac0f7c9977d60b2f270
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-02-28 22:59:37 -0300

    --

--- react-js/18-Projects/book-list/src/components/Author/author.js
+++ react-js/18-Projects/book-list/src/components/Author/author.js
@@ -2,9 +2,10 @@ import React from 'react'
 import './author.css'
 
 export const Author = () => {
+  const authorBook = 'Lloyd Devereux Richards'
   return (
     <div className="author">
-      <h4>Lloyd Devereux Richards</h4>
+      <h4 style={{ color: '#617d98' }}>{authorBook}</h4>
     </div>
   )
 }
