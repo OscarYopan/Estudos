db6a9ed2daf82b6dd052b72d936001fc86d38e45
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-03-03 16:44:28 -0300

    Update booklist.js

--- react-js/18-Projects/book-list/src/components/BookList/booklist.js
+++ react-js/18-Projects/book-list/src/components/BookList/booklist.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Book } from '../Book/book'
+import { firstBook, secondBook, threeBook } from '../../data/data'
 
 import './booklist.css'
 
 export const Booklist = () => {
   return (
     <div className="booklist">
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        image={firstBook.image}
+      />
       <Book />
       <Book />
     </div>
