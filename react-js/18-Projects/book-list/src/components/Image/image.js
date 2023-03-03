a63e870a853e844da15cae7e68b9ac7d80d90ee9
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-03-03 17:10:11 -0300

    Update image.js

--- react-js/18-Projects/book-list/src/components/Image/image.js
+++ react-js/18-Projects/book-list/src/components/Image/image.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { firstBook, secondBook, threeBook } from '../../data/data'
 
 export const Image = props => {
   return (
     <div className="image">
-      <img src="props.image" alt="props.title" />
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/61E29imvhEL._AC_UL600_SR600,400_.jpg"
+        alt="Stone Maidens"
+      />
     </div>
   )
 }
