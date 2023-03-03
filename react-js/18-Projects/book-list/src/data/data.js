e0091337eb24895d8624e15606c73fc4c49ffe0d
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-03-03 16:38:48 -0300

    Update data.js

--- react-js/18-Projects/book-list/src/data/data.js
+++ react-js/18-Projects/book-list/src/data/data.js
@@ -11,3 +11,10 @@ export const secondBook = {
   image:
     'https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg'
 }
+
+export const threeBook = {
+  author: 'Andy Elkerton',
+  title: 'How to Catch a Leprechaun',
+  image:
+    'https://images-na.ssl-images-amazon.com/images/I/81hSK0bkkFL._AC_UL600_SR600,400_.jpg'
+}
