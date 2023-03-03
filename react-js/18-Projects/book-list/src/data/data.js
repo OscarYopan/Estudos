d700a65fd055e87a630154b285e67d60292ee7a0
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-03-03 16:34:14 -0300

    Create data.js

---
+++ react-js/18-Projects/book-list/src/data/data.js
@@ -0,0 +1,13 @@
+export const firstBook = {
+  author: 'Ron DeSantis',
+  title: 'The Courage to Be Free',
+  image:
+    'https://images-na.ssl-images-amazon.com/images/I/81QOUISS7VL._AC_UL600_SR600,400_.jpg'
+}
+
+export const secondBook = {
+  author: 'James Clear',
+  title: 'Atomic Habits',
+  image:
+    'https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg'
+}
