4fdb9925204e9525da03c1c0fc22f9233f55a13e
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-03-07 15:12:17 -0300

    --

--- react-js/18-Projects/book-list/src/components/Author/author.js
+++ react-js/18-Projects/book-list/src/components/Author/author.js
@@ -3,10 +3,10 @@ import './author.css'
 
 const authorBook = 'Lloyd Devereux Richards'
 
-export const Author = () => {
-  return (
-    <div className="author">
-      <h4 style={{ color: '#617d98' }}>{authorBook}</h4>
-    </div>
-  )
-}
+// export const Author = () => {
+//   return (
+//     <div className="author">
+//       <h4 style={{ color: '#617d98' }}>{authorBook}</h4>
+//     </div>
+//   )
+// }
--- react-js/18-Projects/book-list/src/index.js
+++ react-js/18-Projects/book-list/src/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-import { Booklist } from './components/BookList/booklist'
-
 function App() {
   return (
     <div className="App">
@@ -17,3 +15,63 @@ export default App
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(<App />)
+
+const Booklist = () => {
+  return (
+    <div className="booklist">
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        image={firstBook.image}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        image={secondBook.image}
+      />
+      <Book
+        author={threeBook.author}
+        title={threeBook.title}
+        image={threeBook.image}
+      />
+    </div>
+  )
+}
+
+const Book = () => {
+  return (
+    <div className="book">
+      <Image />
+      <Title />
+      <Author />
+    </div>
+  )
+}
+
+const Image = props => {
+  return (
+    <div className="image">
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/61E29imvhEL._AC_UL600_SR600,400_.jpg"
+        alt="Stone Maidens"
+      />
+    </div>
+  )
+}
+
+const Title = () => {
+  const bookTitle = 'Stone Maidens'
+  return (
+    <div className="title">
+      <h1>{bookTitle}</h1>
+    </div>
+  )
+}
+
+const Author = () => {
+  return (
+    <div className="author">
+      <h4 style={{ color: '#617d98' }}>{authorBook}</h4>
+    </div>
+  )
+}
