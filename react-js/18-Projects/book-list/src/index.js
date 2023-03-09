7d5571de28be422904c1b00b27293062b428c0ea
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-03-08 22:04:46 -0300

    Update index.js

--- react-js/18-Projects/book-list/src/index.js
+++ react-js/18-Projects/book-list/src/index.js
@@ -16,6 +16,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(<App />)
 
+//parameters
+const someFun = (param1, param2) => {
+  console.log(param1, param2)
+}
+
+//arguments
+someFun('job', 'developer')
+
 const Booklist = () => {
   return (
     <div className="booklist">
