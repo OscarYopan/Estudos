a2f9dfed749eda5dbf8ee9baee766a94a7286d6b
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-03-10 16:56:50 -0300

    Update index.js

--- react-js/18-Projects/book-list/src/index.js
+++ react-js/18-Projects/book-list/src/index.js
@@ -57,23 +57,23 @@ const Book = props => {
   )
 }
 
-const firstBook = {
-  author: 'Ron DeSantis',
-  title: 'The Courage to Be Free',
-  image:
-    'https://images-na.ssl-images-amazon.com/images/I/81QOUISS7VL._AC_UL600_SR600,400_.jpg'
-}
-
-const secondBook = {
-  author: 'James Clear',
-  title: 'Atomic Habits',
-  image:
-    'https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg'
-}
-
-const threeBook = {
-  author: 'Andy Elkerton',
-  title: 'How to Catch a Leprechaun',
-  image:
-    'https://images-na.ssl-images-amazon.com/images/I/81hSK0bkkFL._AC_UL600_SR600,400_.jpg'
-}
+const books = [
+  {
+    author: 'Ron DeSantis',
+    title: 'The Courage to Be Free',
+    image:
+      'https://images-na.ssl-images-amazon.com/images/I/81QOUISS7VL._AC_UL600_SR600,400_.jpg'
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    image:
+      'https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg'
+  },
+  {
+    author: 'Andy Elkerton',
+    title: 'How to Catch a Leprechaun',
+    image:
+      'https://images-na.ssl-images-amazon.com/images/I/81hSK0bkkFL._AC_UL600_SR600,400_.jpg'
+  }
+]
