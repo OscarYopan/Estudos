5dce0fb56285fd228577955db05d54851f3ce540
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-03-13 14:29:52 -0300

    Update index.js

--- react-js/18-Projects/book-list/src/index.js
+++ react-js/18-Projects/book-list/src/index.js
@@ -37,7 +37,10 @@ const Book = props => {
       <h4>{author}</h4>
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos
-        voluptatum repudiandae.
+        voluptatum repudiandae. Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Iusto nihil quisquam corrupti illo similique. Velit
+        molestiae sint voluptate incidunt tempore laborum eaque? Beatae iusto
+        totam earum dignissimos nihil at fugiat.
       </p>
       <button>Add to Cart!</button>
     </div>
@@ -49,18 +52,21 @@ const books = [
     author: 'Ron DeSantis',
     title: 'The Courage to Be Free',
     image:
-      'https://images-na.ssl-images-amazon.com/images/I/81QOUISS7VL._AC_UL600_SR600,400_.jpg'
+      'https://images-na.ssl-images-amazon.com/images/I/81QOUISS7VL._AC_UL600_SR600,400_.jpg',
+    id: 1
   },
   {
     author: 'James Clear',
     title: 'Atomic Habits',
     image:
-      'https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg'
+      'https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg',
+    id: 2
   },
   {
     author: 'Andy Elkerton',
     title: 'How to Catch a Leprechaun',
     image:
-      'https://images-na.ssl-images-amazon.com/images/I/81hSK0bkkFL._AC_UL600_SR600,400_.jpg'
+      'https://images-na.ssl-images-amazon.com/images/I/81hSK0bkkFL._AC_UL600_SR600,400_.jpg',
+    id: 3
   }
 ]
