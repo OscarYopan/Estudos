1be1b456bc7e6fb293541ed53338d4edba9bb511
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-03-20 20:25:27 -0300

    --

--- chatGPT/gallery/main.js
+++ chatGPT/gallery/main.js
@@ -1,10 +1,41 @@
 const gallery = document.getElementById('gallery')
+const prevBtn = document.getElementById('prevBtn')
+const nextBtn = document.getElementById('nextBtn')
 
-fetch('https://picsum.photos/200')
-  .then(response => {
+let startIndex = 0
+const imagesPerScreen = 3
+
+function displayGallery(images) {
+  gallery.innerHTML = ''
+  for (let i = startIndex; i < startIndex + imagesPerScreen; i++) {
     const img = document.createElement('img')
-    img.src = response.url
+    img.src = images[i % images.length].download_url
     gallery.appendChild(img)
+  }
+}
+
+function updateButtons(images) {
+  prevBtn.disabled = startIndex === 0
+  nextBtn.disabled = startIndex + imagesPerScreen >= images.length
+}
+
+fetch('https://picsum.photos/v2/list')
+  .then(response => response.json())
+  .then(images => {
+    displayGallery(images)
+    updateButtons(images)
+
+    prevBtn.addEventListener('click', () => {
+      startIndex -= imagesPerScreen
+      displayGallery(images)
+      updateButtons(images)
+    })
+
+    nextBtn.addEventListener('click', () => {
+      startIndex += imagesPerScreen
+      displayGallery(images)
+      updateButtons(images)
+    })
   })
   .catch(error => {
     console.log(error)
