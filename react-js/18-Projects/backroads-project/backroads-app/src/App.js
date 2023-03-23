4d3d955a66108e71cae44b369d39fc272467119d
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-03-23 17:49:02 -0300

    Update App.js

--- react-js/18-Projects/backroads-project/backroads-app/src/App.js
+++ react-js/18-Projects/backroads-project/backroads-app/src/App.js
@@ -1,10 +1,21 @@
+import React from 'react'
 import { Navbar } from './components/Navbar'
+import { Hero } from './components/Hero'
+import { About } from './components/About'
+import { Services } from './components/Services'
+import { Tours } from './components/Tours'
+import { Footer } from './components/Footer'
 
 function App() {
   return (
-    <div>
+    <React.Fragment>
       <Navbar />
-    </div>
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </React.Fragment>
   )
 }
 
