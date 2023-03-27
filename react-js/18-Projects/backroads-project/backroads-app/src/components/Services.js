a4f0ff997c3170ae3f0dfcd4068e0f9e1099d533
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-03-27 18:14:36 -0300

    --

--- react-js/18-Projects/backroads-project/backroads-app/src/components/Services.js
+++ react-js/18-Projects/backroads-project/backroads-app/src/components/Services.js
@@ -11,7 +11,7 @@ export const Services = () => {
         {servicesData.map(service => {
           const { id, icon, title, text } = service
           return (
-            <article className="service">
+            <article className="service" key={id}>
               <span className="service-icon">
                 <i className={icon}></i>
               </span>
