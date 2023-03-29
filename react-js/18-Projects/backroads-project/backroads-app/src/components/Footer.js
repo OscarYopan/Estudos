19481b7a2daf6e7922ecb714c35fe967b2d71697
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-03-29 15:55:55 -0300

    Update Footer.js

--- react-js/18-Projects/backroads-project/backroads-app/src/components/Footer.js
+++ react-js/18-Projects/backroads-project/backroads-app/src/components/Footer.js
@@ -18,36 +18,21 @@ export const Footer = () => {
       </ul>
 
       <ul className="footer-icons">
-        <li>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            className="footer-icon"
-            rel="noreferrer"
-          >
-            <i className="fab fa-facebook"></i>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            className="footer-icon"
-            rel="noreferrer"
-          >
-            <i className="fab fa-twitter"></i>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            className="footer-icon"
-            rel="noreferrer"
-          >
-            <i className="fab fa-squarespace"></i>
-          </a>
-        </li>
+        {socialLinks.map(social => {
+          const { id, href, icon } = social
+          return (
+            <li key={id}>
+              <a
+                href={href}
+                target="_blank"
+                className="footer-icon"
+                rel="noreferrer"
+              >
+                <i className={icon}></i>
+              </a>
+            </li>
+          )
+        })}
       </ul>
       <p className="copyright">
         copyright &copy; Backroads travel tours company
