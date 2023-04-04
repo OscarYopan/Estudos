89cbfc2221cc0fd0da86bdc5d86d77075201732d
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-04-04 18:24:31 -0300

    Create SocialLinks.js

---
+++ react-js/18-Projects/backroads-project/backroads-app/src/components/SocialLinks.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const SocialLinks = () => {
+  return (
+    <li key={id}>
+      <a href={href} target="_blank" className="footer-icon" rel="noreferrer">
+        <i className={icon}></i>
+      </a>
+    </li>
+  )
+}
