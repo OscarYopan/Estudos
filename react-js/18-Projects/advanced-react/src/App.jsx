29950fbe1fa55095d9612ed6b9d4191ac8575402
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-04-12 22:12:05 -0300

    Update App.jsx

--- react-js/18-Projects/advanced-react/src/App.jsx
+++ react-js/18-Projects/advanced-react/src/App.jsx
@@ -1,12 +1,12 @@
 import Starter from './tutorial/07-useRef/starter/01-useRef-basics'
 
-import Final from './tutorial/07-useRef/final/01-useRef-basics'
+// import Final from './tutorial/07-useRef/final/01-useRef-basics'
 
 function App() {
   return (
     <div className='container'>
       <Starter />
-      <Final />
+      {/* <Final /> */}
     </div>
   );
 }
