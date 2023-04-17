1b9bf56b519ffda4e30e74b112a7774bbcb96936
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-04-17 19:59:41 -0300

    Update error-example.jsx

--- react-js/18-Projects/advanced-react/src/error-example.jsx
+++ react-js/18-Projects/advanced-react/src/error-example.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
+
 const ErrorExample = () => {
-  let count = 0
+  let [count, setCount] = useState(0)
+
+  const handleClick = () => {
+    setCount(count + 1)
+  }
 
 
   return (
     <div>
-      <h2>{count}</h2>
+      <h2>{setCount}</h2>
+      <button className="btn" type="button" onClick={handleClick(setCount)}>Incriese</button>
     </div>
    )
 };
