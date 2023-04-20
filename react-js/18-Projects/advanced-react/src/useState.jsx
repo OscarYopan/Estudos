9c39bab6ea2ea6ce67feca3068df3e1d7ae9c2fe
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-04-20 18:14:09 -0300

    Update useState.jsx

--- react-js/18-Projects/advanced-react/src/useState.jsx
+++ react-js/18-Projects/advanced-react/src/useState.jsx
@@ -2,21 +2,30 @@ import { useState } from "react";
 import { data } from "./data";
 
 const UseStateEx = () => {
+  const [peaple, setPeaple] = useState(data)
+  
+  function removeItem() {
+
+  }
+
+  function clearAllItems() {
+    
+  }
+
   return (
     <div>
       <h1>UseState Array Exemple</h1>
       <ul>
         {data.map( list => {
           return (
-            <div>
               <li key={list.id} {...list}>
                 <h5>{list.name}</h5>
+                <button>Remove</button>
               </li>
-              <button className="btn">Remove</button>
-            </div>
           )
         })}
       </ul>
+      <button className="btn">Clear All</button>
     </div>
    )
 };
