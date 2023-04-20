8f154403b03a67ed58832e322a52f3f582a9c43d
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-04-20 17:26:41 -0300

    Update useState.jsx

--- react-js/18-Projects/advanced-react/src/useState.jsx
+++ react-js/18-Projects/advanced-react/src/useState.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
+import data from './data'
 
 const UseStateEx = () => {
-
   return (
     <div>
       <h1>Hello World</h1>
+      <ul>
+        {data.map( list => {
+          return (
+            <li key={list.id}>{list.name}</li>
+          )
+        })}
+      </ul>
     </div>
    )
 };
