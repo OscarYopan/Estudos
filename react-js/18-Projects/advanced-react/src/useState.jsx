fbe9c76ead1cbe9e2d8e4a80c8cbb6db82c175a9
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-04-24 15:01:46 -0300

    Update useState.jsx

--- react-js/18-Projects/advanced-react/src/useState.jsx
+++ react-js/18-Projects/advanced-react/src/useState.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
 
 const UseStateEx = () => {
+  const [value, setValue] = useState(0)
+
+  const handleClick = () => {
+    value = setValue(value + 1)
+  }
+
   return (
     <div>
       <h2>UseState Gotcha</h2>
+      <h4>{value}</h4>
+      <button className="btn" onClick={handleClick}>Click</button>
     </div>
    )
 };
