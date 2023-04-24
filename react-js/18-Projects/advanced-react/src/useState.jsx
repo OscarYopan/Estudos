115bbfe612f353054356ea6375a8c7c768acf6f0
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-04-24 14:29:17 -0300

    Update useState.jsx

--- react-js/18-Projects/advanced-react/src/useState.jsx
+++ react-js/18-Projects/advanced-react/src/useState.jsx
@@ -2,13 +2,6 @@ import { useState } from "react";
 import { data } from "./data";
 
 const UseStateEx = () => {
-  const [people, setPeople] = useState(data)
-  
-  const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
-  };
-
   return (
     <div>
       <h1>UseState Oject Example</h1>
