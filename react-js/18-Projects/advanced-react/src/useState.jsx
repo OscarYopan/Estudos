f726731d54c2b2e68803efd192e607530f69c9e5
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-04-24 14:56:03 -0300

    Update useState.jsx

--- react-js/18-Projects/advanced-react/src/useState.jsx
+++ react-js/18-Projects/advanced-react/src/useState.jsx
@@ -1,26 +1,9 @@
 import { useState } from "react";
 
 const UseStateEx = () => {
-  const [person, setPerson] = useState({
-    name: 'Oscar',
-    age: 36,
-    hobby: 'Playing Games'
-  })
-
-  const displayPerson = () => {
-    setPerson({
-      name: 'Amanda', 
-      age: 26, 
-      hobby: 'Go to the beach.'
-    })
-  }
   return (
     <div>
-      <h2>UseState Object Example</h2>
-      <h4><strong>Who:</strong> {person.name}</h4>
-      <h4><strong>Age:</strong> {person.age}</h4>
-      <h4><strong>Hobby:</strong> {person.hobby}</h4>
-      <button className="btn" onClick={displayPerson}>Next</button>
+      <h2>UseState Gotcha</h2>
     </div>
    )
 };
