da042ec5324de39813268c63e2ab775aec20d7d7
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-04-24 14:43:01 -0300

    Update useState.jsx

--- react-js/18-Projects/advanced-react/src/useState.jsx
+++ react-js/18-Projects/advanced-react/src/useState.jsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
-import { people } from "./data";
 
 const UseStateEx = () => {
   const [name, setName] = useState('Oscar')
   const [age, setAge] = useState(36)
-  const [hobby, setHobby] = useState('Gaming')
+  const [hobby, setHobby] = useState('Playing Games')
 
+  const displayPerson = () => {
+    setName('Amanda')
+    setAge(26)
+    setHobby('Swiming on the beach')
+  }
   return (
     <div>
       <h2>UseState Object Example</h2>
+      <h4><strong>Who:</strong> {name}</h4>
+      <h4><strong>Age:</strong> {age}</h4>
+      <h4><strong>Hobby:</strong> {hobby}</h4>
+      <button className="btn" onClick={displayPerson}>Next</button>
     </div>
    )
 };
