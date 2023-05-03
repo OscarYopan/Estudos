fc7ffbc8e722af7a9a2a5f0bd6d414a0e4e8e1d0
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-05-03 20:08:48 -0300

    Update data.js

--- react-js/18-Projects/birthday-reminder/src/data.js
+++ react-js/18-Projects/birthday-reminder/src/data.js
@@ -33,26 +33,5 @@ export const data = [
     age: 29,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg'
-  },
-  {
-    id: 6,
-    name: 'Sean Walsh',
-    age: 34,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg'
-  },
-  {
-    id: 7,
-    name: 'Sean Walsh',
-    age: 34,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg'
-  },
-  {
-    id: 8,
-    name: 'Sean Walsh',
-    age: 34,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg'
   }
 ]
