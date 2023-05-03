d333077e571fc806d92b8fbd3069e03a4f3bb970
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-05-03 20:04:29 -0300

    --

---
+++ react-js/18-Projects/birthday-reminder/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import { data } from './data'
+import { List } from './List'
+
+function App() {
+  const [people, setPeople] = useState(data)
+
+  return (    
+    <main>
+      <section className='container'>
+        <h3>{people.length} Birthdays today</h3>
+        <List people={people} />
+        <button className='btn btn-block' onClick={() => {setPeople([])}}>Clear All</button>
+      </section>
+    </main>
+  )
+}
+
+export default App
