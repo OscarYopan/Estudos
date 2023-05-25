0b7485602e6b1818a36e86966b6f374b22414ab2
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-05-25 14:40:02 -0300

    Update useEffect.jsx

--- react-js/18-Projects/advanced-react/src/useEffect.jsx
+++ react-js/18-Projects/advanced-react/src/useEffect.jsx
@@ -1,16 +1,38 @@
 //https://react.dev/reference/react/useEffect
 
-import React, { useState } from 'react'
+/*
+Reference
+
+    useEffect(setup, dependencies?) 
+
+Usage
+
+    Connecting to an external system
+    Wrapping Effects in custom Hooks
+    Controlling a non-React widget
+    Fetching data with Effects
+    Specifying reactive dependencies
+    Updating state based on previous state from an Effect
+    Removing unnecessary object dependencies
+    Removing unnecessary function dependencies
+    Reading the latest props and state from an Effect
+    Displaying different content on the server and the client 
+*/
+
+import React, { useState, useEffect } from 'react'
 
 export const UseEffectEx = () => {
   const [value, setValue] = useState(0);
 
   const sayHello = () => {
     console.log('Hello Budy');
-    // be carefull, you will have infinite loop
-    setValue(value + 1)
   }
+
   sayHello()
+
+  useEffect(() => {
+    console.log('Hello From useEffect');
+  }, [])
 
   return (
     <div>
