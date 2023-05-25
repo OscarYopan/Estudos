3af8bb6f4d9e03dcb3af4f2454d0f1d5bb6f919a
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-05-25 14:19:30 -0300

    Update useEffect.jsx

--- react-js/18-Projects/advanced-react/src/useEffect.jsx
+++ react-js/18-Projects/advanced-react/src/useEffect.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const UseEffectEx = () => {
+  const [value, setValue] = useState(0);
+
+  const sayHello = () => {
+    console.log('Hello Budy');
+  }
+  sayHello()
+
   return (
-    <div>Use Effect</div>
-  )
+    <div>
+      <h1>value : {value}</h1>
+      <button className='btn' onClick={() => setValue(value + 1)}>
+        click me
+      </button>
+    </div>
+  );
 }
