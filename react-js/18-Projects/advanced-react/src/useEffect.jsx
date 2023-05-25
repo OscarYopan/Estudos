2be4730325cd721341f8a5e5b30897e078af2c37
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-05-25 14:45:07 -0300

    Update useEffect.jsx

--- react-js/18-Projects/advanced-react/src/useEffect.jsx
+++ react-js/18-Projects/advanced-react/src/useEffect.jsx
@@ -1,24 +1,3 @@
-//https://react.dev/reference/react/useEffect
-
-/*
-Reference
-
-    useEffect(setup, dependencies?) 
-
-Usage
-
-    Connecting to an external system
-    Wrapping Effects in custom Hooks
-    Controlling a non-React widget
-    Fetching data with Effects
-    Specifying reactive dependencies
-    Updating state based on previous state from an Effect
-    Removing unnecessary object dependencies
-    Removing unnecessary function dependencies
-    Reading the latest props and state from an Effect
-    Displaying different content on the server and the client 
-*/
-
 import React, { useState, useEffect } from 'react'
 
 export const UseEffectEx = () => {
@@ -43,3 +22,33 @@ export const UseEffectEx = () => {
     </div>
   );
 }
+
+
+//https://react.dev/reference/react/useEffect
+
+/*
+Reference
+
+    useEffect(setup, dependencies?) 
+
+Usage
+
+    Connecting to an external system
+    Wrapping Effects in custom Hooks
+    Controlling a non-React widget
+    Fetching data with Effects
+    Specifying reactive dependencies
+    Updating state based on previous state from an Effect
+    Removing unnecessary object dependencies
+    Removing unnecessary function dependencies
+    Reading the latest props and state from an Effect
+    Displaying different content on the server and the client 
+
+Troubleshooting
+
+    My Effect runs twice when the component mounts
+    My Effect runs after every re-render
+    My Effect keeps re-running in an infinite cycle
+    My cleanup logic runs even though my component didn’t unmount
+    My Effect does something visual, and I see a flicker before it runs 
+*/
