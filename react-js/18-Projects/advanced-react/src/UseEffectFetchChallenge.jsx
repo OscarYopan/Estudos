443ba8492706db6d98f2b76f3ba0d1c1be5ed9b4
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-05-26 15:18:14 -0300

    --

--- react-js/18-Projects/advanced-react/src/UseEffectFetchChallenge.jsx
+++ react-js/18-Projects/advanced-react/src/UseEffectFetchChallenge.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 export const UseEffectFetchChallengeEx = () => {
+  const url = 'https://api.github.com/users';
+
+  const fetchApi = () => {
+    const data = url.map()
+    console.log(data);
+  }
+
+  fetchApi()
+
   return (
-    <div>Ex</div>
+    <div className='fetch-challenge'>
+      <h1>UseEffect Fetch Challenge</h1>
+    </div>
   )
 }
