c5237462b95b558bfd9a9cdfea9a423b1b462bd4
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-06-14 15:15:35 -0300

    --

--- MIni-projects/store-landing-page/src/App.jsx
+++ MIni-projects/store-landing-page/src/App.jsx
@@ -5,6 +5,9 @@ function App() {
   return (
     <>
       <h1>Lá vem ele!</h1>
+      <button>Click</button>
+      <p>Testand CSS de <a href="https://www.google.com/" target='_blank' rel="noreferrer">Link</a> em uma frase.
+      </p>
     </>
   )
 }
