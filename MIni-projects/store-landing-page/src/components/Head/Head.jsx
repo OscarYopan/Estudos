e4ca6cc1646593a67b7d3cecf2e9d5922a7b5e5b
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-06-14 16:42:46 -0300

    --

--- MIni-projects/store-landing-page/src/components/Head/Head.jsx
+++ MIni-projects/store-landing-page/src/components/Head/Head.jsx
@@ -3,7 +3,9 @@ import './head.css'
 export const Head = () => {
   return (
     <div className="head-container">
-      <h1 className='logo'>FakeMarket</h1>
+      <div className='logo'>
+        <a href="#">FakeStore</a>
+      </div>
       <div className='search-box'>
         <input type="text" name="search" id="serach" />
         <button>
@@ -12,7 +14,7 @@ export const Head = () => {
       </div>
       <div className='account'>
         <i>
-          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
         </i>
         <span>Account</span>
       </div>
