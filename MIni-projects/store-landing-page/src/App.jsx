bab942f20fe723ba7012283b3ba04f48f3157869
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-06-16 15:58:39 -0300

    --

--- MIni-projects/store-landing-page/src/App.jsx
+++ MIni-projects/store-landing-page/src/App.jsx
@@ -1,11 +1,10 @@
-import './App.css'
 import { Head } from './components/Head/Head'
 import { NavMenu } from './components/NavMenu/NavMenu'
 
 function App() {
 
   return (
-    <div className='body'>
+    <div className='app'>
       <Head />
       <NavMenu />
     </div>
