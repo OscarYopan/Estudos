3464dfdf0e802ee3e27a5c0fd4a30647e7512b6f
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-06-19 16:43:12 -0300

    --

--- MIni-projects/store-landing-page/src/components/Banner/Banner.jsx
+++ MIni-projects/store-landing-page/src/components/Banner/Banner.jsx
@@ -1,10 +1,11 @@
-import { bannerImages } from '../Data/data'
+// import { bannerImages } from '../Data/data'
 import './banner.css'
 
 export const Banner = () => {
   return (
     <div className='banner-container'>
-
+      <i className="fa-solid fa-left"></i>
+      <i className="fa-solid fa-right"></i>
     </div>
   )
 }
