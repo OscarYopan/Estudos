e3c9174546dd3c53e49560173e193b6510b1ca20
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-06-21 15:51:10 -0300

    --

--- MIni-projects/store-landing-page/src/components/Departments/Departments.jsx
+++ MIni-projects/store-landing-page/src/components/Departments/Departments.jsx
@@ -5,20 +5,24 @@ import depGames from '../../assets/images/dep-games.png'
 export const Departments = () => {
   return (
     <div className="departments">
-      <div className="department games-dep">
-        <h2 className="department-title"><h2>Games</h2></h2>
+      <div className="department" id='games-dep'>
+        <h2 className="department-title">Games</h2>
       </div>
 
-      <div className="department computer-dep">
-        <h2 className="department-title"><h2>Games</h2></h2>
+      <div className="department" id='eletronic-dep'>
+        <h2 className="department-title">Computer</h2>
       </div>
 
-      <div className="department eletronic-dep">
-        <h2 className="department-title"><h2>Games</h2></h2>
+      <div className="department" id='toys-dep'>
+        <h2 className="department-title">Toys</h2>
       </div>
 
-      <div className="department toys-dep">
-        <h2 className="department-title"><h2>Games</h2></h2>
+      <div className="department" id='toys-dep'>
+        <h2 className="department-title">Home</h2>
+      </div>
+
+      <div className="department" id='pet-dep'>
+        <h2 className="department-title">Pet</h2>
       </div>
     </div>
   )
