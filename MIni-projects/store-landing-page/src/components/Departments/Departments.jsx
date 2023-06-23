516870fccb12ad2ec10b28349bb9399abe355353
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-06-23 16:36:22 -0300

    --

--- MIni-projects/store-landing-page/src/components/Departments/Departments.jsx
+++ MIni-projects/store-landing-page/src/components/Departments/Departments.jsx
@@ -23,6 +23,26 @@ export const Departments = () => {
       <div className="department" id='pet-dep'>
         <h2 className="department-title">Pet</h2>
       </div>
+
+      <div className="department" id='games-dep'>
+        <h2 className="department-title">Games</h2>
+      </div>
+
+      <div className="department" id='eletronic-dep'>
+        <h2 className="department-title">Computer</h2>
+      </div>
+
+      <div className="department" id='toys-dep'>
+        <h2 className="department-title">Toys</h2>
+      </div>
+
+      <div className="department" id='home-dep'>
+        <h2 className="department-title">Home</h2>
+      </div>
+
+      <div className="department" id='pet-dep'>
+        <h2 className="department-title">Pet</h2>
+      </div>
     </div>
   )
 }
