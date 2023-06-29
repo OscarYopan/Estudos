00edb1b7aa7e550ede56097c177086f705d7f0a2
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-06-29 15:50:13 -0300

    --

---
+++ MIni-projects/store-landing-page/src/components/Cart/Cart.jsx
@@ -0,0 +1,21 @@
+export const Cart = () => {
+  return (
+    <div>
+      <li>
+        <div className="cart"></div>
+        <h2 className="title">Xiaomi Stick TV 4K</h2>
+        <p className="price">$ 99.00</p>
+      </li>
+      <li>
+        <div className="cart"></div>
+        <h2 className="title"></h2>
+        <p className="price"></p>
+      </li>
+      <li>
+        <div className="cart"></div>
+        <h2 className="title"></h2>
+        <p className="price"></p>
+      </li>
+    </div>
+  )
+}
