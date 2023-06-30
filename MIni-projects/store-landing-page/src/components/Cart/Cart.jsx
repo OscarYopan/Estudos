e0ec8b04e87a55794537f4b10691bca81f0975ae
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-06-30 14:56:18 -0300

    --

--- MIni-projects/store-landing-page/src/components/Cart/Cart.jsx
+++ MIni-projects/store-landing-page/src/components/Cart/Cart.jsx
@@ -1,12 +1,17 @@
 import './cart.css'
+import cart01 from '../../assets/images/cart_img_01.png'
 
 export const Cart = () => {
   return (
     <div className="cart-container">
       <li>
-        <div className="cart"></div>
-        <h2 className="title">Xiaomi Stick TV 4K</h2>
-        <p className="price">$ 99.00</p>
+        <div className="cart">
+          <img src={cart01} alt="" />
+        </div>
+        <div className="cart-information">
+          <h2 className="title">Xiaomi Stick TV 4K</h2>
+          <p className="price">$ 99.00</p>
+        </div>
       </li>
       <li>
         <div className="cart"></div>
