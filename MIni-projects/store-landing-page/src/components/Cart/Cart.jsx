cd942dd3050d3df48037af6868ca9f59a8e55c56
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-06-30 15:08:22 -0300

    --

--- MIni-projects/store-landing-page/src/components/Cart/Cart.jsx
+++ MIni-projects/store-landing-page/src/components/Cart/Cart.jsx
@@ -1,5 +1,7 @@
 import './cart.css'
 import cart01 from '../../assets/images/cart_img_01.png'
+import cart02 from '../../assets/images/cart_img_02.png'
+import cart03 from '../../assets/images/cart_img_03.png'
 
 export const Cart = () => {
   return (
@@ -10,19 +12,28 @@ export const Cart = () => {
         </div>
         <div className="cart-information">
           <h2 className="title">Xiaomi Stick TV 4K</h2>
-          <p className="price">$ 99.00</p>
+          <p className="price">$ 99.90</p>
         </div>
       </li>
       <li>
-        <div className="cart"></div>
-        <h2 className="title">Produt Name</h2>
-        <p className="price">$ 1.99</p>
+        <div className="cart">
+          <img src={cart02} alt="" />
+        </div>
+        <div className="cart-information">
+          <h2 className="title">Smart Watch Amazfit Bip 3</h2>
+          <p className="price">$ 69.90</p>
+        </div>
       </li>
       <li>
-        <div className="cart"></div>
-        <h2 className="title">Produt Name</h2>
-        <p className="price">$ 1.99</p>
+        <div className="cart">
+          <img src={cart03} alt="" />
+        </div>
+        <div className="cart-information">
+          <h2 className="title">Xbox Series X</h2>
+          <p className="price">$ 99.00</p>
+        </div>
       </li>
+      <span><a href="#">See more ...</a></span>
     </div>
   )
 }
